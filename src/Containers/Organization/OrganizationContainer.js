4ea795328b20bb9221e0a826029a386fa8f4d96b
Ezequiel Galardi <eze3769@gmail.com>
2022-01-20 15:54:40 -0300

    refactor: correct organization structure

---
+++ src/Containers/Organization/OrganizationContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Organization from '../../Components/Organization/Organization';
+import mockLogo from "./LOGO-SOMOS-MAS.png"
+
+const OrganizationContainer = () => {
+    //datos mockups hasta armar la estructura definitiva
+    const organizationMock = {
+        name: "Somos Más",
+        image: mockLogo,
+        shortDescription: "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y debinserción social."
+      }
+
+  return (
+        <>
+            <Organization organizationMock={organizationMock}/>
+        </>    
+    );
+};
+
+export default OrganizationContainer;
