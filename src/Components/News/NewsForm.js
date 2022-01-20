26a3f5333868b4cac15eae1e8edc235f5c579ae8
Rosti <pablorostirollatenis@gmail.com>
2022-01-19 23:17:09 -0300

    .

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -2,55 +2,88 @@ import React from "react";
 import "../../Components/FormStyles.css";
 import { ErrorMessage, Form, Formik } from "formik";
 import * as Yup from "yup";
+import ContainerFormCard from "../ContainerFormCard";
 
 const NewsForm = () => {
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationNewSchema}
-      onSubmit={(formData) => console.log(formData)}
-    >
-      {(formik) => (
-        <Form className="form-container" onSubmit={formik.handleSubmit}>
-          <input
-            className="input-field"
-            type="text"
-            placeholder="Título..."
-            {...formik.getFieldProps("title")}
-          ></input>
-          <ErrorMessage
-            name="title"
-            className="formik_error"
-            component="span"
-          />
-          <input
-            className="input-field"
-            type="text"
-            placeholder="Contenido..."
-            {...formik.getFieldProps("content")}
-          ></input>
-          <ErrorMessage
-            name="content"
-            className="formik_error"
-            component="span"
-          />
-          <select
-            className="select-field"
-            {...formik.getFieldProps("category")}
-          >
-            <option value="" disabled>
-              Select category
-            </option>
-            <option value="1">Demo option 1</option>
-            <option value="2">Demo option 2</option>
-            <option value="3">Demo option 3</option>
-          </select>
-          <button className="submit-btn" type="submit">
-            Send
-          </button>
-        </Form>
-      )}
-    </Formik>
+    <ContainerFormCard>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationNewSchema}
+        onSubmit={(formData) => console.log(formData)}
+      >
+        {(formik) => (
+          <Form className="p-4" onSubmit={formik.handleSubmit}>
+            <div className="mb-1">
+              <label className="form-label">Título</label>
+              <input
+                className="form-control form-control-sm w-100"
+                type="text"
+                placeholder="Ingrese un título"
+                {...formik.getFieldProps("title")}
+              ></input>
+              <ErrorMessage
+                name="title"
+                className="formik_error"
+                component="span"
+              />
+            </div>
+            <div className="mb-1">
+              <label className="form-label">Contenido</label>
+              <input
+                className="form-control form-control-sm"
+                type="text"
+                placeholder="Contenido..."
+                {...formik.getFieldProps("content")}
+              ></input>
+            </div>
+            <ErrorMessage
+              name="content"
+              className="formik_error"
+              component="span"
+            />
+
+            <div className="mb-1">
+              <label className="form-label">Categoría</label>
+              <select
+                class="form-select form-select-sm"
+                aria-label="Default select example"
+                {...formik.getFieldProps("category")}
+              >
+                <option selected>Selecciona una categoria</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <ErrorMessage
+              name="category"
+              className="formik_error"
+              component="span"
+            />
+
+            <div className="mb-1">
+              <label className="form-label">Imagen</label>
+              <input
+                className="form-control form-control-sm"
+                type="file"
+                placeholder="imagen..."
+                {...formik.getFieldProps("image")}
+              ></input>
+            </div>
+            <ErrorMessage
+              name="image"
+              className="formik_error"
+              component="span"
+            />
+
+            <button className="btn btn-primary w-100 mt-2" type="submit">
+              Agregar novedad
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </ContainerFormCard>
   );
 };
 
@@ -62,6 +95,9 @@ const initialValues = {
   category: "",
 };
 const validationNewSchema = Yup.object({
-  title: Yup.string().required("El titulo es obligatorio"),
+  title: Yup.string()
+    .min(4, "Debe contener al menos 4 caracteres")
+    .required("El titulo es obligatorio"),
   content: Yup.string().required("El contenido es obligatorio"),
+  category: Yup.string().required("La categoría es obligatoria"),
 });
