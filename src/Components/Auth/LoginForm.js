1605634c10a2ce6c8475f8f3dd1a2d8f731e55bd
ivan eduardo salas <ivanedusalas@gmail.com>
2022-01-20 12:53:35 -0300

    updating login component, using yup for validations

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,82 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+import "../FormStyles.css";
+import { Formik, ErrorMessage, Field, errors } from "formik";
+import * as Yup from "yup";
 
 const LoginForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        email: '',
-        password: ''
-    });
+  //   ALOJA EL TOKEN UNA VEZ AUTENTICADO EL USUARIO
+  //   PENDIENTE A LA PETICIÓN HTTP
+  //
+  //   const handleLogin = (values) => {
+  //     const url = ""
+  //     const body = {
+  //       email: values.email,
+  //       password: values.password,
+  //     };
+  //     axios.post(url, body).then((response) => {
+  //       localStorage.setItem("token", response.data.token);
+  //     });
+  //   };
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+  const startValues = {
+    email: "",
+    password: "",
+  };
 
-    return (
+  return (
+    <Formik
+      initialValues={startValues}
+      validationSchema={Yup.object().shape({
+        email: Yup.string().required("Field Required"),
+        password: Yup.string()
+          .max(6, "Password must have at least 6 characters")
+          .required("Field required")
+          .matches(
+            /[!@#$%^&*]/,
+            "Password must have at least one special character"
+          ),
+      })}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors, handleSubmit }) => (
         <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
+          <Field
+            className="input-field"
+            type="email"
+            name="email"
+            placeholder="Enter email"
+          />
+          <ErrorMessage
+            name="email"
+            component={() => (
+              <div class="alert alert-danger" role="alert">
+                {errors.email}
+              </div>
+            )}
+          />
+          <Field
+            className="input-field"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+          />
+          <ErrorMessage
+            name="password"
+            component={() => (
+              <div class="alert alert-danger" role="alert">
+                {errors.password}
+              </div>
+            )}
+          />
+          <button className="submit-btn" type="submit">
+            Log In
+          </button>
         </form>
-    );
-}
- 
+      )}
+    </Formik>
+  );
+};
+
 export default LoginForm;
