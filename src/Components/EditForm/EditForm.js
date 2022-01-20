cca7ea74751979ef9db87c89c6c2fee703fecb13
Emiliano <emiliano.quiroga093@gmail.com>
2022-01-20 19:40:46 -0300

    Created component file

---
+++ src/Components/EditForm/EditForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import { Formik, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const EditForm = () => {
+    return (
+        <div className="container">
+            <h1>Edit form</h1>
+        </div>
+    )
+}
+
+export default EditForm;
