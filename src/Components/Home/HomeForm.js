44e4e20b9f9450fe8f0da527dcb98d804ab16101
Ezequiel Galardi <eze3769@gmail.com>
2022-01-21 16:55:42 -0300

    refactor: change some stuffs in HomeForm, commited before pull on developer branch

--- src/Components/Home/HomeForm.js
+++ src/Components/Home/HomeForm.js
@@ -1,48 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 
 const HomeForm = () => {
+    const [image1, setImage1] = useState()
+    const [image2, setImage2] = useState()
+    const [image3, setImage3] = useState()
 
     const formSchema = Yup.object().shape({
-        name: Yup.string()
+        welcome_text: Yup.string()
         .required("Campo Requerido")
-        .min(3, `Mínimo 3 caracteres`)
-        .max(25, `Máximo 25 caracteres`),
-        image: Yup.string()
-        .min(5, `Mínimo 5 caracteres`)
-        .max(25, `Máximo 25 caracteres`)
-        .required("Campo Requerido"),
-        shortDescription: Yup.string()
-        .required("Campo Requerido")
-        .max(120, `Máximo 120 caracteres`),
+        .min(20, `Mínimo 20 caracteres`)
+        .max(255, `Máximo 255 caracteres`),
     });
 
     const formik = useFormik({
         initialValues: {
-          firstName: '',
-          lastName: '',
-          email: '',
+            welcome_text: '',
+            slides:[{
+                image:'',
+                description:''
+            }]
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
           },
         validationSchema:formSchema,
+        
     });
+    const handleImageChange = (state, e) =>{
+        if (e.target.files && e.target.files[0]) {
+            state(URL.createObjectURL(e.target.files[0]));
+        }
+    }
 
     
   return (
       <>
           <form onSubmit={formik.handleSubmit}>
-             <label htmlFor="firstName">First Name</label>
+             <label htmlFor="welcome_text">Bienvenida:</label>
                 <input
-                  id="firstName"
-                  name="firstName"
+                  id="welcome_text"
+                  name="welcome_text"
                   type="text"
                   onChange={formik.handleChange}
-                  value={formik.values.firstName}
+                  value={formik.values.welcome_text}
                 />
+                <div className="row text-center">
+                    <div className="col-md-4 mb-4">
+                        <div className="z-depth-1-half mb-4">
+                            <img src={image1} className="img-fluid" alt="example placeholder"/>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <button className="btn btn-primary btn-rounded float-left">
+                                <span>Choose file</span>
+                                <input 
+                                    type="file" 
+                                    name="slice1.image"
+                                    onChange={e=>{handleImageChange(setImage1,e)}}
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <div className="z-depth-1-half mb-4">
+                            <img src={image1} className="img-fluid" alt="example placeholder"/>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <div className="btn btn-mdb-color btn-rounded float-left">
+                                <span>Choose file</span>
+                                <input 
+                                    type="file" 
+                                    name="slice1.image"
+                                    onChange={e=>{handleImageChange(setImage2,e)}}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <div className="z-depth-1-half mb-4">
+                            <img src={image1} className="img-fluid" alt="example placeholder"/>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <div className="btn btn-mdb-color btn-rounded float-left">
+                                <span>Choose file</span>
+                                <input 
+                                    type="file" 
+                                    name="slice1.image"
+                                    onChange={e=>{handleImageChange(setImage3,e)}}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <label htmlFor="lastName">Last Name</label>
                 <input
                   id="lastName"
