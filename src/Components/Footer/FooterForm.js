f595f8bc9353ebde713b78b2b5bcd826b9811527
Guido Greco <guidogreco@Guidos-MacBook-Pro.local>
2022-01-21 18:33:41 -0300

    fix: estilos bootstrap y validacion

--- src/Components/Footer/FooterForm.js
+++ src/Components/Footer/FooterForm.js
@@ -2,115 +2,132 @@ import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import "../FormStyles.css";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import "bootstrap/dist/css/bootstrap.css";
 
 const FooterForm = () => {
   const [formSend, setFormSend] = useState(false);
-  const [valueNombre, setValueNombre] = useLocalStorage("nombre", "");
-  const [valueApellido, setValueApellido] = useLocalStorage("apellido", "");
-  const [valueCorreo, setValueCorreo] = useLocalStorage("correo", "");
+  const [valueName, setValueName] = useLocalStorage("nombre", "");
+  const [valueSurname, setValueSurname] = useLocalStorage("apellido", "");
+  const [valueEmail, setValueEmail] = useLocalStorage("correo", "");
+
+  const formDisplayToggle = () => {
+    setFormSend(!formSend);
+  };
 
   return (
-    <Formik
-      initialValues={{
-        nombre: "",
-        apellido: "",
-        correo: "",
-      }}
-      validate={(valores) => {
-        let errores = {};
-        //Validacion nombre
-        if (!valores.nombre) {
-          errores.nombre = "Por favor ingrese un nombre";
-        } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
-          errores.nombre = "El nombre solo puede contener letras y espacios";
-        }
+    <React.Fragment>
+      <Formik
+        initialValues={{
+          name: "",
+          surname: "",
+          email: "",
+        }}
+        validate={(valores) => {
+          let errores = {};
+          //Validacion nombre
+          if (!valores.name) {
+            errores.name = "Por favor ingrese un nombre";
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)) {
+            errores.name = "El nombre solo puede contener letras y espacios";
+          }
 
-        //Validacion apellido
-        if (!valores.apellido) {
-          errores.apellido = "Por favor ingrese un apellido";
-        } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.apellido)) {
-          errores.apellido =
-            "El apellido solo puede contener letras y espacios";
-        }
+          //Validacion apellido
+          if (!valores.surname) {
+            errores.surname = "Por favor ingrese un apellido";
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.surname)) {
+            errores.surname =
+              "El apellido solo puede contener letras y espacios";
+          }
 
-        //Validacion correo
-        if (!valores.correo) {
-          errores.correo = "Por favor ingrese un correo electronico";
-        } else if (
-          !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
-            valores.correo
-          )
-        ) {
-          errores.correo =
-            "El correo solo puede contener letras, numeros, puntos, guines y guion bajo";
-        }
+          //Validacion correo
+          if (!valores.email) {
+            errores.email = "Por favor ingrese un correo electronico";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.email
+            )
+          ) {
+            errores.email =
+              "El correo solo puede contener letras, numeros, puntos, guines y guion bajo";
+          }
 
-        return errores;
-      }}
-      onSubmit={(valores, { resetForm }) => {
-        console.log(valores);
-        setFormSend(true);
-        setTimeout(() => setFormSend(false), 5000); //ver validacion
-        resetForm();
-        //los valores se colocan en localstorage
-        setValueNombre(valores.nombre);
-        setValueApellido(valores.apellido);
-        setValueCorreo(valores.correo);
-      }}
-    >
-      {({ errors }) => (
-        <Form className="form-container">
-          <div>
-            <label htmlFor="nombre">Nombre</label>
-            <Field
-              className="input-field"
-              type="text"
-              id="nombre"
-              name="nombre"
-              placeholder="Nombre"
-
-              //   onChange={(event) => setValue(event.targe.value)}
-            />
-            <ErrorMessage
-              name="nombre"
-              component={() => <p>{errors.nombre}</p>}
-            />
-          </div>
-          <div>
-            <label htmlFor="apellido">Apellido</label>
-            <Field
-              className="input-field"
-              type="text"
-              id="apellido"
-              name="apellido"
-              placeholder="Apellido"
-            />
-            <ErrorMessage
-              name="apellido"
-              component={() => <p>{errors.apellido}</p>}
-            />
-          </div>
-          <div>
-            <label htmlFor="correo">Correo</label>
-            <Field
-              className="input-field"
-              type="text"
-              id="correo"
-              name="correo"
-              placeholder="correo"
-            />
-            <ErrorMessage
-              name="correo"
-              component={() => <p>{errors.correo}</p>}
-            />
-          </div>
-          <button type="submit" className="submit-btn">
-            Enviar
-          </button>
-          {formSend && <p>Formulario enviado con exito</p>}
-        </Form>
+          return errores;
+        }}
+        onSubmit={(valores, { resetForm }) => {
+          console.log(valores);
+          setFormSend(true);
+          // setTimeout(() => setFormSend(false), 5000); ver validacion
+          resetForm();
+          //los valores se colocan en localstorage
+          setValueName(valores.name);
+          setValueSurname(valores.surname);
+          setValueEmail(valores.email);
+        }}
+      >
+        {({ errors }) => (
+          <Form className={formSend ? "formNotDisplay" : "form-container"}>
+            <div className="mt-3">
+              <label htmlFor="name" className="form-label">
+                Nombre
+              </label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Nombre"
+                className="form-control"
+              />
+              <ErrorMessage
+                name="name"
+                component={() => <p className="error">{errors.name}</p>}
+              />
+            </div>
+            <div className="mt-3">
+              <label htmlFor="surname" className="form-label">
+                Apellido
+              </label>
+              <Field
+                type="text"
+                id="surname"
+                name="surname"
+                placeholder="Apellido"
+                className="form-control"
+              />
+              <ErrorMessage
+                name="surname"
+                component={() => <p className="error">{errors.surname}</p>}
+              />
+            </div>
+            <div className="mt-3">
+              <label htmlFor="email" className="form-label">
+                Correo
+              </label>
+              <Field
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Correo"
+                className="form-control"
+              />
+              <ErrorMessage
+                name="email"
+                component={() => <p className="error">{errors.email}</p>}
+              />
+            </div>
+            <button
+              onClick={formDisplayToggle}
+              type="submit"
+              className="btn btn-primary"
+            >
+              Enviar
+            </button>
+          </Form>
+        )}
+      </Formik>
+      {formSend && (
+        <p className="formSubmitted">Formulario enviado con éxito</p>
       )}
-    </Formik>
+    </React.Fragment>
   );
 };
 
