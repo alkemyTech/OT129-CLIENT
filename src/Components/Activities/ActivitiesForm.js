f9ef45e43536c1029552ba9b179037c2bb135f20
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-01-20 23:49:01 -0300

    add Formik form and partial validation

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,32 +1,67 @@
-import React, { useState } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import '../FormStyles.css';
 
-const ActivitiesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
-
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+const ActivitiesForm = ({ activities }) => {   
     
+    const validationSchema = Yup.object({
+        name: Yup
+            .string()
+            .required('The activity name is required'),
+        image: Yup
+            .mixed()
+            .required('A file is required')
+            .test('fileType', "Unsupported file type", (value) => {               
+                if (value) return ["image/jpeg", "image/png"].includes(value.type)
+                else return true
+            })
+    })
+
+    let initialValues = {
+        name: activities?.name || "",
+        description: activities?.description || "",
+        image: activities?.image || "",
+    }
+
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+            className="form-container">
+            {({ errors, setFieldValue }) => (
+                <Form>
+                    <Field
+                        id="name"
+                        type="text"
+                        name="name"
+                        placeholder="Activity Title"
+                    />
+                    <ErrorMessage name='name' component={() => (<div className="error">{errors.name}</div>)} className='alert-danger' />
+                    <Field
+                        id="description"
+                        type="text"
+                        name="description"
+                        placeholder="Write some activity description"
+                    />
+                    
+                    <input
+                        id="image"
+                        type="file"
+                        name="image"
+                        accept="image/png, image/jpeg"
+                        onChange={(event) => {
+                            setFieldValue("image", event.currentTarget.files[0]);
+                        }}
+                    />
+                    <ErrorMessage name='file' component={() => (<div className="error">{errors.image}</div>)} className='alert-danger' />
+                    <button className="submit-btn" type="submit">Send</button>
+                </Form>
+            )}
+        </Formik>
     );
 }
- 
+
 export default ActivitiesForm;
