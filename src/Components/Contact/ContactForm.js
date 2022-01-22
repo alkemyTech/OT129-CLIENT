da71def2bdbee6cd70fd936c76b56390bff4fa87
Ana Galli <anaamgalli@gmail.com>
2022-01-21 22:55:19 -0300

    eslint errors fixed in file and requested changes addressed

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,73 +1,72 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import "./ContactForm.css";
+import {
+  Formik, Form, Field, ErrorMessage
+} from "formik";
+import "../FormStyles.css";
 import * as Yup from "yup";
 
-const ErrorComponent = (props) => {
-    return(
-        <p>{props.children}</p>
-    );
-}
+const ErrorComponent = (props) => (
+  <p>{props.children}</p>
+);
+
+const initialValues = {
+  name: "",
+  email: "",
+  phone: "",
+  message: ""
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Requerido"),
+  email: Yup.string().email("Formato de email incorrecto").required("Requerido"),
+  phone: Yup.string().matches(/^\d+$/, "Formato incorrecto").min(8, "Debe ingresar un mínimo de 8 números.").required("Requerido"),
+  message: Yup.string().required("Requerido")
+});
 
 const ContactForm = () => {
+  const onSubmit = (values, { resetForm }) => {
+    resetForm();
+  };
 
-    const initialValues = {
-        name: "",
-        email: "",
-        phone: "",
-        message: ""
-    }
-
-    const onSubmit = ( values, {resetForm} ) => {
-        resetForm();
-    }
-
-    const validationSchema = Yup.object({
-        name: Yup.string().required("Requerido"),
-        email: Yup.string().email("Formato de email incorrecto").required("Requerido"),
-        phone: Yup.string().matches(/^\d+$/, "Formato incorrecto").min(8, "Debe ingresar un mínimo de 8 números.").required("Requerido"),
-        message: Yup.string().required("Requerido")
-    });
-
-    return(
-        <Formik
-            initialValues = {initialValues}
-            validationSchema = {validationSchema}
-            onSubmit = {onSubmit}
-        >
-            <Form className="form-container">
-                <Field
-                    type="text"
-                    name="name"
-                    placeholder="Nombre"
-                    className="input-field"
-                />
-                <ErrorMessage name="name" component={ErrorComponent} />
-                <Field
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                    className="input-field"
-                />
-                <ErrorMessage name="email" component={ErrorComponent} />
-                <Field
-                    type="tel"
-                    name="phone"
-                    placeholder="Teléfono"
-                    className="input-field"
-                />
-                <ErrorMessage name="phone" component={ErrorComponent} />
-                <Field
-                    as="textarea"
-                    name="message"
-                    placeholder="Escribe tu mensaje"
-                    className="input-field"
-                />
-                <ErrorMessage name="message" component={ErrorComponent} />
-                <button type="submit" className="submit-btn">Enviar</button>
-            </Form>
-        </Formik>
-    );
-}
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form className="form-container">
+        <Field
+          type="text"
+          name="name"
+          placeholder="Nombre"
+          className="input-field"
+        />
+        <ErrorMessage name="name" component={ErrorComponent} />
+        <Field
+          type="text"
+          name="email"
+          placeholder="Email"
+          className="input-field"
+        />
+        <ErrorMessage name="email" component={ErrorComponent} />
+        <Field
+          type="tel"
+          name="phone"
+          placeholder="Teléfono"
+          className="input-field"
+        />
+        <ErrorMessage name="phone" component={ErrorComponent} />
+        <Field
+          as="textarea"
+          name="message"
+          placeholder="Escribe tu mensaje"
+          className="input-field"
+        />
+        <ErrorMessage name="message" component={ErrorComponent} />
+        <button type="submit" className="submit-btn">Enviar</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default ContactForm;
