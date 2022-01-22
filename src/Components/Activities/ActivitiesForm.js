b640208577994176c3bee958bb0b071ad91d6bd8
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-01-21 22:00:47 -0300

    fix: Eslint errors resolved

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,68 +1,83 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import React from 'react';
+import {
+  Formik, Field, Form, ErrorMessage
+} from 'formik';
+import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import '../FormStyles.css';
 
-const ActivitiesForm = ({ activities }) => {   
-    
-    const validationSchema = Yup.object({
-        name: Yup
-            .string()
-            .required('The activity name is required'),
-        image: Yup
-            .mixed()
-            .required('A file is required')
-            .test('fileType', "Unsupported file type", (value) => {               
-                if (value) return ["image/jpeg", "image/png"].includes(value.type)
-                else return true
-            })
-    })
+// eslint-disable-next-line react/prop-types
+const Error = ({ children }) => <div className="error">{children}</div>;
 
-    let initialValues = {
-        name: activities?.name || "",
-        description: activities?.description || "",
-        image: activities?.image || "",
-    }
+const ActivitiesForm = ({ activity }) => {
+  const validationSchema = Yup.object({
+    name: Yup
+      .string()
+      .required('The activity name is required'),
+    image: Yup
+      .mixed()
+      .required('A file is required')
+      .test('fileType', "Unsupported file type", (value) => {
+        if (value) return ["image/jpeg", "image/png"].includes(value.type);
+        return true;
+      })
+  });
 
+  const initialValues = {
+    name: activity?.name || "",
+    description: activity?.description || "",
+    image: activity?.image || "",
+  };
 
-    return (
-        <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={(values) => {
-                console.log(values);
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        // eslint-disable-next-line no-console
+        console.log(values);
+      }}
+      className="form-container"
+    >
+      {({ setFieldValue }) => (
+        <Form>
+          <Field
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Activity Title"
+          />
+          <ErrorMessage name="name" component={Error} className="alert-danger" />
+          <Field
+            id="description"
+            type="text"
+            name="description"
+            placeholder="Write some activity description"
+          />
+
+          <input
+            id="image"
+            type="file"
+            name="image"
+            accept="image/png, image/jpeg"
+            onChange={(event) => {
+              setFieldValue("image", event.currentTarget.files[0]);
             }}
-            className="form-container">
-            {({ errors, setFieldValue }) => (
-                <Form>
-                    <Field
-                        id="name"
-                        type="text"
-                        name="name"
-                        placeholder="Activity Title"
-                    />
-                    <ErrorMessage name='name' component={() => (<div className="error">{errors.name}</div>)} className='alert-danger' />
-                    <Field
-                        id="description"
-                        type="text"
-                        name="description"
-                        placeholder="Write some activity description"
-                    />
-                    
-                    <input
-                        id="image"
-                        type="file"
-                        name="image"
-                        accept="image/png, image/jpeg"
-                        onChange={(event) => {
-                            setFieldValue("image", event.currentTarget.files[0]);
-                        }}
-                    />
-                    <ErrorMessage name='file' component={() => (<div className="error">{errors.image}</div>)} className='alert-danger' />
-                    <button className="submit-btn" type="submit">Send</button>
-                </Form>
-            )}
-        </Formik>
-    );
-}
+          />
+          <ErrorMessage name="file" component={Error} className="alert-danger" />
+          <button className="submit-btn" type="submit">Send</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+ActivitiesForm.propTypes = {
+  activity: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default ActivitiesForm;
--- src/Components/Organization/Organization.js
+++ src/Components/Organization/Organization.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import "./Organization.css";
 import mockLogo from "./LOGO-SOMOS-MAS.png";
 import { Link } from "react-router-dom";
@@ -15,36 +14,34 @@ const Organization = () => {
 
   return (
     <div className="container-bg">
-      <Container className="card-bg">
-        <h1 className="pb-2">Organización</h1>
-        <div className="d-flex flex-column align-items-start">
+      <h1 className="pb-2">Organización</h1>
+      <div className="d-flex flex-column align-items-start">
+        <h5 className="text-center">
+          <b>Nombre: </b>
+          {dataMock.name}
+        </h5>
+        <div className="d-flex align-items-center">
           <h5 className="text-center">
-            <b>Nombre: </b>
-            {dataMock.name}
+            <b>Imágen: </b>
           </h5>
-          <div className="d-flex align-items-center">
-            <h5 className="text-center">
-              <b>Imágen: </b>
-            </h5>
-            <img src={dataMock.image} alt={dataMock.name} />
-          </div>
-          <h5>
-            <b>Descripción corta: </b>
-          </h5>
-          <h5>{dataMock.shortDescription}</h5>
+          <img src={dataMock.image} alt={dataMock.name} />
         </div>
-        <div className="d-flex justify-content-between my-3">
-          <Link
-            to="falta agregar la ruta o algún método para volver"
-            className="btn btn-secondary "
-          >
-            Volver
-          </Link>
-          <Link to="/backoffice/organization/edit" className="btn btn-primary ">
-            Editar
-          </Link>
-        </div>
-      </Container>
+        <h5>
+          <b>Descripción corta: </b>
+        </h5>
+        <h5>{dataMock.shortDescription}</h5>
+      </div>
+      <div className="d-flex justify-content-between my-3">
+        <Link
+          to="falta agregar la ruta o algún método para volver"
+          className="btn btn-secondary "
+        >
+          Volver
+        </Link>
+        <Link to="/backoffice/organization/edit" className="btn btn-primary ">
+          Editar
+        </Link>
+      </div>
     </div>
   );
 };
