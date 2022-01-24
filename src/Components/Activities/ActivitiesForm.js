7a87c39bd4083d6baf908518d5aaa15439a5c023
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-01-24 01:57:21 -0300

    add toBase64 async function & CKE insted description

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import {
   Formik, Field, Form, ErrorMessage
@@ -5,79 +6,103 @@ import {
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import '../FormStyles.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-// eslint-disable-next-line react/prop-types
-const Error = ({ children }) => <div className="error">{children}</div>;
+const toBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = reject;
+  });
 
-const ActivitiesForm = ({ activity }) => {
+const ActivitiesForm = ({ activity = {} }) => {
+  const initialValues = {
+    id: activity?.id || "",
+    name: activity?.name || "",
+    description: activity?.description || "",
+    image: activity?.image || "",
+  };
   const validationSchema = Yup.object({
     name: Yup
       .string()
-      .required('The activity name is required'),
+      .required('El nombre de la actividad es obligatorio'),
     image: Yup
       .mixed()
-      .required('A file is required')
-      .test('fileType', "Unsupported file type", (value) => {
+      .required('Debe adjuntar una imagen')
+      .test('fileType', "La extensión del archivo no es soportado", (value) => {
         if (value) return ["image/jpeg", "image/png"].includes(value.type);
         return true;
       })
   });
 
-  const initialValues = {
-    name: activity?.name || "",
-    description: activity?.description || "",
-    image: activity?.image || "",
-  };
-
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values) => {
-        // eslint-disable-next-line no-console
-        console.log(values);
-      }}
-      className="form-container"
-    >
-      {({ setFieldValue }) => (
-        <Form>
-          <Field
-            id="name"
-            type="text"
-            name="name"
-            placeholder="Activity Title"
-          />
-          <ErrorMessage name="name" component={Error} className="alert-danger" />
-          <Field
-            id="description"
-            type="text"
-            name="description"
-            placeholder="Write some activity description"
-          />
-
-          <input
-            id="image"
-            type="file"
-            name="image"
-            accept="image/png, image/jpeg"
-            onChange={(event) => {
-              setFieldValue("image", event.currentTarget.files[0]);
-            }}
-          />
-          <ErrorMessage name="file" component={Error} className="alert-danger" />
-          <button className="submit-btn" type="submit">Send</button>
-        </Form>
-      )}
-    </Formik>
+    <div className="container mt-4">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (formData) => {
+          console.log(formData);
+          const resultBase = await toBase64(formData.image);
+          const newActivity = { ...formData, image: resultBase };
+          console.log(newActivity);
+        }}
+      >
+        {(formik) => (
+          <Form className="row justify-content-center align-items-center">
+            <div className="col-sm-6 ">
+              <div className="input-group mb-3">
+                <Field
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Nombre de la actividad"
+                  className="p-2 w-100"
+                />
+                <ErrorMessage name="name" className="alert-danger" />
+              </div>
+              <div className="input-group mb-3">
+                <CKEditor
+                  id="description"
+                  editor={ClassicEditor}
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    formik.setFieldValue("description", data);
+                  }}
+                  className="p-2 w-75"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  id="image"
+                  type="file"
+                  name="image"
+                  accept="image/png, image/jpeg"
+                  className="w-100"
+                  onChange={(event) => {
+                    formik.setFieldValue("image", event.currentTarget.files[0]);
+                  }}
+                />
+                <ErrorMessage name="image" className="alert-danger" />
+              </div>
+              <button className="submit-btn" type="submit">{activity?.id ? "EDITAR" : "CREAR"}</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
 ActivitiesForm.propTypes = {
+  // eslint-disable-next-line react/require-default-props
   activity: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     description: PropTypes.string,
     image: PropTypes.string.isRequired,
-  }).isRequired,
+  }),
 };
 
 export default ActivitiesForm;
