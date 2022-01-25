b39e778e3e3064607128766e34f521d39e71977b
Ivan Salas <ivanedusalas@gmail.com>
2022-01-25 08:18:32 -0300

    updating the whole component

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,61 +1,170 @@
-import React, { useState } from "react";
+src/Components/Auth/LoginForm.jsimport React, { useState } from "react";
 import "../FormStyles.css";
 
-const UserForm = () => {
-  const [initialValues, setInitialValues] = useState({
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required("Field Required")
+    .min(4, "Must have at least 4 characters"),
+  email: Yup.string().required("Field Required").email("Is not a valid format"),
+  role_id: Yup.string().required("Field Required"),
+  profile_image: Yup.string().required("Field Required"),
+});
+
+const isDesktop = window.innerWidth > 768;
+
+const UserForm = ({  id = '' }) => {
+  const isAddMode = id === "";
+
+  const startValues = {
     name: "",
     email: "",
-    roleId: "",
-  });
+    role_id: "",
+    profile_image: "",
+  };
 
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setInitialValues({ ...initialValues, name: e.target.value });
-    }
-    if (e.target.name === "email") {
-      setInitialValues({ ...initialValues, email: e.target.value });
+  const [user, setUser] = useState({});
+
+  const onSubmit = async (formValues) => {
+    if (isAddMode) {
+      const result = await toBase64(formValues.profile_image);
+      const dataUser = {
+        name: formValues.name,
+        email: formValues.email,
+        role_id: formValues.role_id,
+        profile_image: result,
+      };
+      axios.post("http://ongapi.alkemy.org/api/users", dataUser).then((user)=>{
+        setUser(user.data)
+      })
+    } else {
+      const result = await toBase64(formValues.profile_image);
+      const dataUser = {
+        name: formValues.name,
+        email: formValues.email,
+        role_id: formValues.role_id,
+        profile_image: result,
+      };
+      axios.put(`http://ongapi.alkemy.org/api/users/${id}`, dataUser).then((user)=>{
+        setUser(user.data)
+      })
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
+  const SetFields = (setValue) => {
+    useEffect(() => { 
+      if (!isAddMode) {
+        axios.get(`http://ongapi.alkemy.org/api/users/${id}`).then((user) => { 
+          setUser(user.data)})
+          if (user) {
+            const fields = ["name", "email", "role_id", "profile_image"];
+            fields.forEach((field) => setValue(field, user[field]));
+          }
+      }
+    }, []);
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        name="name"
-        placeholder="Name"
-        type="text"
-        value={initialValues.name || ""}
-        onChange={handleChange}
-      />
-      <input
-        className="input-field"
-        name="email"
-        placeholder="Email"
-        type="text"
-        value={initialValues.description || ""}
-        onChange={handleChange}
-      />
-      <select
-        className="input-field"
-        value={initialValues.roleId || ""}
-        onChange={(e) => setInitialValues({ ...initialValues, roleId: e.target.value })}
+    <div
+      className={`${
+        isDesktop ? "w-25" : "w-75"
+      } mt-5 m-auto border border-light shadow-lg rounded px-4 py-4`}
+    >
+      <Formik
+        initialValues={startValues}
+        validationSchema={validationSchema}
+        onSubmit={(formValues) => {
+          onSubmit(formValues);
+        }}
       >
-        <option disabled value="">
-          Select the role
-        </option>
-        <option value="1">Admin</option>
-        <option value="2">User</option>
-      </select>
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
+        {({ errors, handleSubmit, setFieldValue }) => {
+          SetFields(setFieldValue);
+
+          return (
+            <form className="mt-3" onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name:
+                </label>
+                <Field
+                  className="form-control mb-3"
+                  type="text"
+                  name="name"
+                  placeholder="Enter name"
+                />
+                <ErrorMessage
+                  name="name"
+                  component={() => (
+                    <div className="alert alert-danger" role="alert">
+                      {errors.name}
+                    </div>
+                  )}
+                />
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email:
+                </label>
+                <Field
+                  className="form-control mb-3"
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component={() => (
+                    <div className="alert alert-danger" role="alert">
+                      {errors.email}
+                    </div>
+                  )}
+                />
+              </div>
+              <input
+                className="form-control mb-3"
+                name="profile_image"
+                type="file"
+                onChange={(event) => {
+                  setFieldValue("profile_image", event.currentTarget.files[0]);
+                }}
+              />
+              <ErrorMessage
+                name="profile_image"
+                component={() => (
+                  <div className="alert alert-danger" role="alert">
+                    {errors.profile_image}
+                  </div>
+                )}
+                className="text-danger"
+              />
+              <div className="mb-3">
+                <Field className="form-select" name="role_id" as="select">
+                  <option defaultValue>Choose</option>
+                  <option value="1">Admin</option>
+                  <option value="2">User</option>
+                </Field>
+              </div>
+              <ErrorMessage
+                name="role_id"
+                component={() => (
+                  <div className="alert alert-danger" role="alert">
+                    {errors.role_id}
+                  </div>
+                )}
+              />
+
+              <div className="mb-3">
+                <button className="btn btn-primary w-100" type="submit">
+                  Send
+                </button>
+              </div>
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 };
+
+
 
 export default UserForm;
