ddca2473f9df88c228f0f9f3c41d83b8a0124088
Ana Galli <anaamgalli@gmail.com>
2022-01-25 00:38:18 -0300

    form created using formik, ckeditor implemented and validations added

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,49 +1,78 @@
-import React, { useState } from "react";
+import React from "react";
+import { Form, Field, ErrorMessage, FormikProvider, useFormik } from "formik";
+import * as Yup from "yup";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import PropTypes from "prop-types";
 import "../FormStyles.css";
 
+const ErrorComponent = (props) => {
+  return <p>{props.children}</p>;
+};
+
+const initialValues = {
+  name: "",
+  description: "",
+  image: "",
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .min(4, "El título debe contener un mínimo de 4 carácteres.")
+    .required("Requerido"),
+  image: Yup.mixed()
+    .required("Requerido")
+    .test("fileType", "Formato incorrecto. Sólo se aceptan archivos .jpg, .jpeg, .png", (value) => {
+      if (value) return ["image/jpeg", "image/jpg", "image/png"].includes(value.type);
+    }),
+  description: Yup.string().required("Requerido"),
+});
+
 const CategoriesForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: "",
-    description: "",
-  });
-
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setInitialValues({ ...initialValues, name: e.target.value });
-    }
-    if (e.target.name === "description") {
-      setInitialValues({ ...initialValues, description: e.target.value });
-    }
+  const onSubmit = (values, { resetForm }) => {
+    resetForm();
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-  };
+  const formik = useFormik({ initialValues, onSubmit, validationSchema });
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        name="name"
-        placeholder="Title"
-        type="text"
-        value={initialValues.name}
-        onChange={handleChange}
-      />
-      <input
-        className="input-field"
-        name="description"
-        placeholder="Write some description"
-        type="text"
-        value={initialValues.description}
-        onChange={handleChange}
-      />
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
+    <FormikProvider
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      value={formik}
+      onSubmit={onSubmit}
+    >
+      <Form className="form-container">
+        <Field className="input-field" name="name" placeholder="Título categoría" type="text" />
+        <ErrorMessage component={ErrorComponent} name="name" />
+        <input
+          className="input-field"
+          name="image"
+          type="file"
+          onChange={(e) => {
+            formik.setFieldValue("image", e.currentTarget.files[0]);
+          }}
+        />
+        <ErrorMessage component={ErrorComponent} name="image" />
+        <CKEditor
+          config={{ placeholder: "Ingrese una descripción" }}
+          editor={ClassicEditor}
+          name="description"
+          onChange={(e, editor) => {
+            formik.setFieldValue("description", editor.getData());
+          }}
+        />
+        <ErrorMessage component={ErrorComponent} name="description" />
+        <button className="submit-btn" type="submit">
+          Enviar
+        </button>
+      </Form>
+    </FormikProvider>
   );
+};
+
+ErrorComponent.propTypes = {
+  children: PropTypes.string.isRequired,
 };
 
 export default CategoriesForm;
