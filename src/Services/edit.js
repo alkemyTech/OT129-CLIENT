c7812f630b1cdfd5c730b48e387182d0ad7268c4
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-01-26 17:00:08 -0300

    fix: modify function

--- src/Services/edit.js
+++ src/Services/edit.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const edit = async (route, data) => {
-  let result = await axios.post(`http://ongapi.alkemy.org/api/${route}/${data.id}`, data, {
+  let result = await axios.put(`http://ongapi.alkemy.org/api/${route}/${data.id}`, data, {
     headers: {
       "Content-Type": "application/json",
       group: "129",
