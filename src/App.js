b87ad5450217819f01ec3cd615f2b994d99fc2bc
Emiliano <82418581+emquiroga@users.noreply.github.com>
2022-01-26 10:05:20 -0300

    OT129 34 formulario de actualizacion (#19)
    * added testing route and component
    
    * Created component file
    
    * working on component
    
    * pulling last merge in dev
    
    * component fixed with eslint
    
    * modified eslinterc for bug fixing, updated package.json, eslinted app
    
    * fixed component with eslint
    
    * starting component
    
    * fixed errors with eslint
    
    * updated app for component rendering
    
    * working on validations
    
    * validations working
    
    * added CKEditor dep
    
    * added edit form container and validated editform
    
    * Links validations updated
    
    * Updated ecmaVersion from 12 to "latest"
    
    * Added img preview, changed mockup logo url
    
    * Deleted redux counter from app
    
    * Updated links from project document
    
    * Fixed bug due ecmaVersion in eslint.js
    
    * added CKEditor dep
    
    * Added CKEditor, input type file and previews
    
    * Removed bootstrap, fixed initialValues
    
    * Removed initialValues const

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Counter from "./features/counter/Counter";
-import logo from "./logo.svg";
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
 import CategoriesForm from "./Components/Categories/CategoriesForm";
 import NewsForm from "./Components/News/NewsForm";
@@ -13,8 +11,9 @@ import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
 import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
 import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
-import Organization from "./Components/Organization/Organization";
+import OrganizationContainer from "./Containers/Organization/OrganizationContainer";
 import RegisterForm from "./Components/Auth/RegisterForm";
+import EditOrganizationContainer from "./Containers/EditOrganization/EditOrganizationContainer";
 import ActivitiesDetailsContainer from "./Components/Activities/Detail/ActivitiesDetailsContainer";
 import HomeForm from "./Components/Home/HomeForm";
 import ContactForm from "./Components/Contact/ContactForm";
@@ -33,7 +32,9 @@ function App() {
           <Route exact component={CategoriesForm} path="/create-category" />
           <Route exact component={NewsForm} path="/backoffice/create-news" />
           <Route exact component={SlidesForm} path="/backoffice/create-slide" />
-          <Route exact component={Organization} path="/backoffice/organization" />
+          <Route exact component={OrganizationContainer} path="/backoffice/organization" />
+          <Route exact component={EditOrganizationContainer} path="/backoffice/organization/edit" />
+          <Route exact component={OrganizationContainer} path="/backoffice/organization" />
           <Route exact component={HomeForm} path="/backoffice/home" />
           <Route exact component={TestimonialForm} path="/create-testimonials" />
           <Route exact component={UserForm} path="/create-user" />
@@ -45,53 +46,6 @@ function App() {
           <Route exact component={ContactForm} path="/contact" />
         </Switch>
       </BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img alt="logo" className="App-logo" src={logo} />
-          <Counter />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <span>
-            <span>Learn </span>
-            <a
-              className="App-link"
-              href="https://reactjs.org/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              React
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Redux
-            </a>
-            <span>, </span>
-            <a
-              className="App-link"
-              href="https://redux-toolkit.js.org/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Redux Toolkit
-            </a>
-            ,<span> and </span>
-            <a
-              className="App-link"
-              href="https://react-redux.js.org/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              React Redux
-            </a>
-          </span>
-        </header>
-      </div>
     </>
   );
 }
