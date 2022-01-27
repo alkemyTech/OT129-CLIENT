f0dde748b7b4458bca08d8e2b94e76f126dde50d
Emiliano <emiliano.quiroga093@gmail.com>
2022-01-27 10:49:51 -0300

    Created container with API service

---
+++ src/Containers/HomeSlide/HomeSlide.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+import HomeSlides from "../../Components/Home/HomeSlides";
+
+const axios = require("axios");
+
+const HomeContainer = () => {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://ongapi.alkemy.org/api/slides")
+      .then((response) => {
+        const result = response.data.data;
+        const slides = result.map((slide) => {
+          const { id, image, name, description } = slide;
+
+          return { id, image, name, description };
+        });
+
+        setSlides(slides);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <HomeSlides data={slides} />;
+};
+
+export default HomeContainer;
