d17400fab9daba5cad6f493e314cd95b6a11ca12
Ana Galli <97997049+anagalli@users.noreply.github.com>
2022-01-26 22:33:49 -0300

    [OT129 28] Formulario de creación y edición de categorías (#20)
    * branch created and ckeditor installed
    
    * ckeditor classic editor installed
    
    * form created using formik, ckeditor implemented and validations added
    
    * validations added
    
    * fix: eslint bugs fixed
    
    * requests added
    
    * Categories container added. Refactoring
    
    * imported utils and api requests services functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
+import CategoriesContainer from "./Containers/Categories/CategoriesContainer";
 import NewsForm from "./Components/News/NewsForm";
 import SlidesForm from "./Components/Slides/SlidesForm";
 import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
@@ -30,7 +30,7 @@ function App() {
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route exact component={ActivitiesForm} path="/create-activity" />
           <Route exact component={ActivitiesDetailsContainer} path="/activities/:id" />
-          <Route exact component={CategoriesForm} path="/create-category" />
+          <Route exact component={CategoriesContainer} path="/create-category" />
           <Route exact component={NewsForm} path="/backoffice/create-news" />
           <Route exact component={SlidesForm} path="/backoffice/create-slide" />
           <Route exact component={EditOrganizationContainer} path="/backoffice/organization/edit" />
