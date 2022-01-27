27710065c9a578975ef30c24f82d7d606a80572e
Ana Galli <anaamgalli@gmail.com>
2022-01-27 10:31:18 -0300

    initial component development and styles

--- src/Components/Titles/Titles.js
+++ src/Components/Titles/Titles.js
@@ -1,7 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Titles = () => {
-  return <></>;
+import "./Titles.css";
+import background from "./background-title.jpg";
+
+const Titles = ({ title, image = background }) => {
+  return (
+    <div className="container-fluid banner">
+      <img alt={title} className="image-banner" src={image} />
+      <div className="row">
+        <div className="container text-banner">
+          <div className="row row-banner">
+            <div className="col-12">
+              <h1 className="title-banner">{title}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Titles.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default Titles;
