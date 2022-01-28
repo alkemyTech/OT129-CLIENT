aee34e62998691ca4fa76f32e18ae6bebaeebfbc
malks14 <greco.guido@hotmai.com>
2022-01-28 13:34:42 -0300

    fix: iconos a los botones

--- src/Components/Back/BackUserItem.js
+++ src/Components/Back/BackUserItem.js
@@ -2,18 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.css";
 
-const BackUserItem = ({ user }) => {
+const BackUserItem = ({ user, onEdit, onDeleteClick }) => {
   return (
     <tr>
       <td>{user.name}</td>
       <td>{user.email}</td>
       <td>
-        <button className="btn btn-warning" type="button">
+        <button className="btn btn-warning" onClick={(event) => onEdit(event, user)} type="button">
+          <i className="fas fa-user-edit" />
           Editar
         </button>
       </td>
       <td>
-        <button className="btn btn-danger" type="button">
+        <button className="btn btn-danger" onClick={() => onDeleteClick(user.id)} type="button">
+          <i className="fas fa-eraser" />
           Eliminar
         </button>
       </td>
