f174d170daf16713b112eb547d95c0033746f0d3
Ana Galli <anaamgalli@gmail.com>
2022-01-28 11:24:33 -0300

    feat: titles component developed

--- src/Components/Titles/Titles.js
+++ src/Components/Titles/Titles.js
@@ -4,15 +4,16 @@ import PropTypes from "prop-types";
 import "./Titles.css";
 import background from "./background-title.jpg";
 
-const Titles = ({ title, image = background }) => {
+const Titles = ({ title, description, image = background }) => {
   return (
-    <div className="container-fluid banner">
+    <div className="container-fluid d-flex justify-content-center align-items-center banner py-5">
       <img alt={title} className="image-banner" src={image} />
       <div className="row">
         <div className="container text-banner">
           <div className="row row-banner">
             <div className="col-12">
-              <h1 className="title-banner">{title}</h1>
+              <h1 className="mb-0 title-banner">{title}</h1>
+              {description && <p className="mb-0 mt-3">{description}</p>}
             </div>
           </div>
         </div>
@@ -23,6 +24,7 @@ const Titles = ({ title, image = background }) => {
 
 Titles.propTypes = {
   title: PropTypes.string,
+  description: PropTypes.string,
   image: PropTypes.string,
 };
 
