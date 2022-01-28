5bbacd1ea6e077a485ae8a1e4466d6405174f179
malks14 <greco.guido@hotmai.com>
2022-01-28 18:36:49 -0300

    feat: se agrego mockData en Page/Contenedor

--- src/Pages/BackOfficeUserPage.js
+++ src/Pages/BackOfficeUserPage.js
@@ -2,7 +2,33 @@ import React from "react";
 
 import BackUsersList from "../Components/Back/BackUsersList";
 
-import dataMock from "./mock-user.json";
+const dataMock = [
+  {
+    id: 1,
+    name: "Matias",
+    email: "matias@matias.com",
+  },
+  {
+    id: 2,
+    name: "Enzo",
+    email: "enzo@enzo.com",
+  },
+  {
+    id: 3,
+    name: "Franco",
+    email: "franco@franco.com",
+  },
+  {
+    id: 4,
+    name: "Julian",
+    email: "julian@julian.com",
+  },
+  {
+    id: 5,
+    name: "Robert",
+    email: "robert@robert.com",
+  },
+];
 
 const BackOfficeUserPage = () => {
   const linkUrl = "/backoffice/users/create";
