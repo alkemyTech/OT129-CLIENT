502a385cfe02ec2063c2fec5a35fedbfd66db2c7
Ana Galli <anaamgalli@gmail.com>
2022-01-28 23:13:26 -0300

    component and container developed and styles added

--- src/Components/Categories/CategoriesList.js
+++ src/Components/Categories/CategoriesList.js
@@ -1,6 +1,38 @@
 import React from "react";
-const CategoriesList = () => {
-  return <></>;
+import PropTypes from "prop-types";
+
+const CategoriesList = ({ data }) => {
+  return (
+    <table className="table table-striped table-list">
+      <thead className="thead-list">
+        <tr>
+          <th>Título</th>
+          <th>Creado</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((el, i) => (
+          <tr key={i}>
+            <td>{el.name}</td>
+            <td>{el.created_at}</td>
+            <td>
+              <button className="btn-list btn-edit" title="Editar">
+                <i className="fas fa-pencil-alt" />
+              </button>
+              <button className="btn-list btn-delete" title="Eliminar">
+                <i className="far fa-trash-alt" />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+CategoriesList.propTypes = {
+  data: PropTypes.array,
 };
 
 export default CategoriesList;
--- src/Containers/Categories/CategoriesListContainer.js
+++ src/Containers/Categories/CategoriesListContainer.js
@@ -1,6 +1,51 @@
 import React from "react";
+
+import { TitleNav } from "../../Components/TitleNav/TitleNav";
+import CategoriesList from "../../Components/Categories/CategoriesList";
+
+const dataCategories = [
+  {
+    id: 1541,
+    name: "edit category",
+    description: "<p>description</p>",
+    image: "http://ongapi.alkemy.org/storage/SKxzZsLtC2.jpeg",
+    parent_category_id: null,
+    created_at: "2022-01-06T14:47:01.000000Z",
+    updated_at: "2022-01-27T00:18:27.000000Z",
+    deleted_at: null,
+    group_id: null,
+  },
+  {
+    id: 1544,
+    name: "cat create",
+    description: "asd",
+    image: "http://ongapi.alkemy.org/storage/xO1qQ4xAUr.png",
+    parent_category_id: null,
+    created_at: "2022-01-06T20:13:01.000000Z",
+    updated_at: "2022-01-06T20:13:01.000000Z",
+    deleted_at: null,
+    group_id: null,
+  },
+  {
+    id: 1552,
+    name: "probando probando",
+    description: "nueva prueba para evidencia",
+    image: null,
+    parent_category_id: null,
+    created_at: "2022-01-07T03:31:07.000000Z",
+    updated_at: "2022-01-07T03:31:07.000000Z",
+    deleted_at: null,
+    group_id: null,
+  },
+];
+
 const CategoriesListContainer = () => {
-  return <></>;
+  return (
+    <div className="container mt-5">
+      <TitleNav link="/backoffice/categories/create" linkTitle="Crear" title="Categorías" />
+      <CategoriesList data={dataCategories} />
+    </div>
+  );
 };
 
 export default CategoriesListContainer;
