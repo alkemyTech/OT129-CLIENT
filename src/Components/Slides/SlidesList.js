e5908b8b594aa113fb87031d9f810077abc1b0bb
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-01-30 11:28:40 -0300

    delete unuseful fragment

--- src/Components/Slides/SlidesList.js
+++ src/Components/Slides/SlidesList.js
@@ -5,29 +5,27 @@ import SlidesItem from "./SlidesItem";
 
 const SlidesList = ({ slides }) => {
   return (
-    <>
-      <table className="table table-hover table-striped mt-3">
-        <thead>
-          <tr>
-            <th scope="col">Titulo</th>
-            <th className="text-center" scope="col">
-              Imagen
-            </th>
-            <th className="text-center" scope="col">
-              Orden
-            </th>
-            <th className="text-center" scope="col">
-              Acciones
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {slides?.map((slide) => (
-            <SlidesItem key={slide.id} slide={slide} />
-          ))}
-        </tbody>
-      </table>
-    </>
+    <table className="table table-hover table-striped mt-3">
+      <thead>
+        <tr>
+          <th scope="col">Titulo</th>
+          <th className="text-center" scope="col">
+            Imagen
+          </th>
+          <th className="text-center" scope="col">
+            Orden
+          </th>
+          <th className="text-center" scope="col">
+            Acciones
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {slides?.map((slide) => (
+          <SlidesItem key={slide.id} slide={slide} />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
