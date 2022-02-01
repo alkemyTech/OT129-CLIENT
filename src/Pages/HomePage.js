4728e90c9f5aa723e801e0017b4dbc10a8a5f8a4
Your Name <you@example.com>
2022-02-01 18:07:15 -0300

    se añadio el componente card

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import HomeSlide from "../Containers/HomeSlide/HomeSlide";
+import ContainerCard from "../Containers/CardContainer/ContainerCard";
 
 const HomePage = () => {
+  const cardNews = "Cards Desde la API Novedades";
+
   return (
     <div className="container">
       <h1 className="text-center mt-3">Home Page</h1>
       <HomeSlide />
+      <ContainerCard card={cardNews} />
     </div>
   );
 };
 
+HomePage.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+  }),
+};
 export default HomePage;
