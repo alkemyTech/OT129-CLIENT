c7a4a6b1de56311f2c238cd9d7488dfc66071f08
malks14 <greco.guido@hotmai.com>
2022-02-01 16:13:23 -0300

    feat: creacion peticion post

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,19 +2,31 @@ import axios from "axios";
 
 import config from "./axiosConfig";
 
+const Authenticate = () => {
+  const storage = localStorage.getItem("token");
+
+  if (storage) {
+    const header = { Authentication: `Bearer ${storage}` };
+
+    return header;
+  } else {
+    return null;
+  }
+};
+
 const privateConfig = {
   ...config,
   headers: {
     ...config.headers,
-    Authorization: "",
+    Authorization: Authenticate.Authentication,
   },
   body: {},
 };
 
 const instance = axios.create(privateConfig);
 
-export const get = (url, id = null) => {
-  let processedURL = id ? `${url}/${id}` : url;
+export const post = (endpoint, id = null, data) => {
+  let processedURL = (id ? `${endpoint}/${id}` : url, data);
 
-  return instance.get(processedURL);
+  return instance.post(processedURL);
 };
