a2101a99192ecb3cdb8b5ae63eef163898b3ea47
Ezequiel Galardi <eze3769@gmail.com>
2022-01-31 23:13:51 -0300

    refactor: added authorization header

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,6 +2,18 @@ import axios from "axios";
 
 import axiosInstance from "./axiosInstance";
 
+export const authenticate = () => {
+  const storage = localStorage.getItem("token");
+
+  if (storage) {
+    const header = { Authentication: `Bearer${storage}` };
+
+    return header;
+  } else {
+    return null;
+  }
+};
+
 export const Get = () => {
   axios
     .get("https://jsonplaceholder.typicode.com/users", config)
@@ -12,3 +24,5 @@ export const Get = () => {
 export const put = (endpoint, data, id = null) => {
   axiosInstance.put(id ? `${endpoint}/${id}` : endpoint, data);
 };
+
+axios.defaults.headers.put["Autorization"] = authenticate().Authentication;
