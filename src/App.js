18f9fcafbb464020c3a5338783651fbe5c46133b
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-02 14:16:35 -0300

    Updated import due rename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ProjectsForm from "./Components/Projects/ProjectsForm";
 import SlidesContainer from "./Components/Slides/SlidesContainer";
 import FooterForm from "./Components/Footer/FooterForm";
 import RegisterForm from "./Components/Auth/RegisterForm";
-import EditOrganizationContainer from "./Containers/EditOrganization/EditOrganizationContainer";
+import EditOrganization from "./Containers/EditOrganization/EditOrganization";
 import ActivitiesDetailsContainer from "./Components/Activities/Detail/ActivitiesDetailsContainer";
 import CategoriesListContainer from "./Containers/Categories/CategoriesListContainer";
 import HomeForm from "./Components/Home/HomeForm";
@@ -52,7 +52,7 @@ function App() {
           <Route exact component={NewsDetailsContainer} path="/novedades/:id" />
           <Route exact component={SlidesForm} path="/backoffice/create-slide" />
           <Route exact component={SlidesContainer} path="/backoffice/slides" />
-          <Route exact component={EditOrganizationContainer} path="/backoffice/organization/edit" />
+          <Route exact component={EditOrganization} path="/backoffice/organization/edit" />
           <Route exact component={HomeForm} path="/backoffice/home" />
           <Route exact component={ActivitiesListContainer} path="/backoffice/activities" />
           <Route exact component={MembersContainer} path="/backoffice/members" />
