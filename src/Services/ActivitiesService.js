ad72bb8b934cb89e6e4c85189fd7068c6190d3d8
Ezequiel Galardi <eze3769@gmail.com>
2022-02-02 14:13:47 -0300

    feat: updated endpoints in activitiesService.js

--- src/Services/ActivitiesService.js
+++ src/Services/ActivitiesService.js
@@ -1,6 +1,6 @@
 import { get, post, put } from "./publicApiService";
 
-const ENDPOINT = "activities";
+const ENDPOINT = process.env.REACT_APP_API_ACTIVITIES;
 
 export const getAtivities = () => {
   return get(ENDPOINT);
