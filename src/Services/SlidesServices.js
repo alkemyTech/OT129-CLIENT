278aec73f2ca62facb1c235e1d6c6428d99081d2
Your Name <you@example.com>
2022-02-02 19:26:54 -0300

    edicion de slidesService, creacion de .env.example

--- src/Services/SlidesServices.js
+++ src/Services/SlidesServices.js
@@ -1,7 +1,14 @@
-import { get } from "./publicApiService";
+import { get, post, put } from "./publicApiService";
 
-const ENDPOINT = "slides";
+const ENDPOINT = "REACT_APP_API_SLIDES_ENDPOINT";
 
 export const getSlides = () => {
   return get(ENDPOINT);
+};
+export const createSlides = () => {
+  return post(ENDPOINT);
+};
+
+export const editSlides = () => {
+  return put(ENDPOINT);
 };
