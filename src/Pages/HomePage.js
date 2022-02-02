4a0cf1046c3501bafb700859f8184c3cb778a3c0
Your Name <you@example.com>
2022-02-02 18:56:27 -0300

    actualizacion a developmen

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,11 +6,13 @@ import ContainerCard from "../Containers/CardContainer/ContainerCard";
 
 const HomePage = () => {
   const cardNews = "Cards Desde la API Novedades";
+  const welcomeText = "Texto de Bienvenida";
 
   return (
     <div className="container">
       <h1 className="text-center mt-3">Home Page</h1>
       <HomeSlide />
+      <h2> {welcomeText}</h2>
       <ContainerCard card={cardNews} />
     </div>
   );
@@ -21,6 +23,8 @@ HomePage.propTypes = {
     name: PropTypes.string,
     description: PropTypes.string,
     image: PropTypes.string,
+    welcome_text: PropTypes.string,
   }),
 };
+
 export default HomePage;
