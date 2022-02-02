3ba36332ea62d0702dc77e36d8bfbcb3a60d9e87
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-02 14:19:23 -0300

    fix: arreglo build

--- src/Components/LazyLoad/LazyImage.js
+++ src/Components/LazyLoad/LazyImage.js
@@ -5,7 +5,6 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const LazyImage = ({ src, alt }) => {
-
   return <LazyLoadImage alt={alt} effect="opacity" src={src} />;
 };
 
