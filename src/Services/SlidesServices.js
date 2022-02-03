61ec480caa4fde74990c69faf0ad93dbda45dbf8
Your Name <you@example.com>
2022-02-03 13:42:18 -0300

    onli public Api Service specified)

--- src/Services/SlidesServices.js
+++ src/Services/SlidesServices.js
@@ -1,23 +1,15 @@
-import { patch } from "./publicApiService";
-import { get, post, put } from "./privateApiService";
+import { get, post, put } from "./publicApiService";
 
 const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT;
 
-export const patchSlides = () => {
-  return patch(ENDPOINT);
+export const getSlides = () => {
+  return get(ENDPOINT);
 };
 
-// The token is needed for this method
-export const getSlides = (id) => {
-  return get(`${ENDPOINT}/${id}`);
-};
-
-// The token is needed for this method
 export const postSlides = (data) => {
   return post(ENDPOINT, data);
 };
 
-// The token is needed for this method
 export const putSlides = (id, data) => {
   return put(`${ENDPOINT}/${id}`, data);
 };
