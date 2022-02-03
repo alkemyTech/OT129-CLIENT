ca13410f806bef8de57315540a8c65a64cba8cc3
Your Name <you@example.com>
2022-02-02 21:27:43 -0300

    correcion de la variable de entorno

--- src/Services/SlidesServices.js
+++ src/Services/SlidesServices.js
@@ -1,6 +1,6 @@
 import { get, post, put } from "./publicApiService";
 
-const ENDPOINT = "REACT_APP_API_SLIDES_ENDPOINT";
+const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT;
 
 export const getSlides = () => {
   return get(ENDPOINT);
