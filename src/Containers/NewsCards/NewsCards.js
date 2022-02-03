49b7b1fe909d1241040fda2937bfbd330712aa65
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-03 11:34:18 -0300

    Deleted mockup data

--- src/Containers/NewsCards/NewsCards.js
+++ src/Containers/NewsCards/NewsCards.js
@@ -3,34 +3,7 @@ import PropTypes from "prop-types";
 
 import Card from "../../Components/Card/Card";
 
-const data1 = [
-  {
-    id: 1,
-    name: "Prueba1",
-    description: "Esta es una actividad de prueba, se reemplazarán luego por las traídas de la API",
-    image: "http://ongapi.alkemy.org/storage/hh2Lju770B.png",
-  },
-  {
-    id: 2,
-    name: "Prueba2",
-    description: "Esta es una actividad de prueba, se reemplazarán luego por las traídas de la API",
-    image: "http://ongapi.alkemy.org/storage/hh2Lju770B.png",
-  },
-  {
-    id: 3,
-    name: "PlaceHolder",
-    description: "Esta es una actividad de prueba, se reemplazarán luego por las traídas de la API",
-    image: "",
-  },
-  {
-    id: 4,
-    name: "Prueba4",
-    description: "Esta es una actividad de prueba, se reemplazarán luego por las traídas de la API",
-    image: "http://ongapi.alkemy.org/storage/hh2Lju770B.png",
-  },
-];
-
-const NewsCards = ({ data = data1 }) => {
+const NewsCards = ({ data }) => {
   return (
     <div className="container my-5 d-grid gap-3">
       <div className="row">
