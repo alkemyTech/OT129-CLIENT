a1ee772aaae9689b22ae5549867464876078ea4a
Ana Galli <97997049+anagalli@users.noreply.github.com>
2022-02-02 21:50:27 -0300

    [OT129 74] Desarrollar servicio y métodos reutilizables para realizar una petición GET a las rutas públicas de la API (#62)
    * get request added
    
    * feat: get request added in news page to show data

--- src/Containers/CardContainer/ShowCard.js
+++ src/Containers/CardContainer/ShowCard.js
@@ -1,17 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./ShowCard.css";
 
 const ShowCard = ({ title, image, description }) => {
   return (
     <div className="card">
       {image ? (
-        <img alt="Card image cap" className="card-img-top" src={image} />
+        <img alt="Card image cap" className="card-img-top img-card" src={image} />
       ) : (
-        <img alt="Placeholder" className="card-img-top" src="\images\placeholder\emptyImage.png" />
+        <img
+          alt="Placeholder"
+          className="card-img-top img-card"
+          src="\images\placeholder\emptyImage.png"
+        />
       )}
       <div className="card-body">
         <h5 className="card-title text-uppercase">{title}</h5>
         <p className="card-text">{description}</p>
+        <a className="general-btn fill-btn text-decoration-none" href="#">
+          Leer más
+        </a>
       </div>
     </div>
   );
