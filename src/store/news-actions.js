fbda14d7dbb124b811e8330289c7c9d13112b696
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-03 20:51:05 -0300

    feat: actions for news

---
+++ src/store/news-actions.js
@@ -0,0 +1,33 @@
+
+import { newsActions } from "./news-slice";
+
+import { getNews } from "../Services/NewsService";
+//action creator thunk
+
+export const fetchNewsData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+        const response = await getNews()
+
+        if (!response.ok) {
+            throw new Error('Could not fetch news data')
+        }
+
+        const data = await response.json();
+
+        return data;
+    }
+
+    try {
+        const newsData =  await fetchData();
+        dispatch(newsActions.showList({
+            newsList: newsData.items || [],
+        }));
+    } catch (error) {
+        //error handling
+    }
+    
+  };
+
+ 
+};
