a66e881c3041b97742e7569a79ad87a72fe36a0b
Your Name <you@example.com>
2022-02-03 12:34:11 -0300

    SlidesService fix

--- src/Services/SlidesServices.js
+++ src/Services/SlidesServices.js
@@ -1,15 +1,26 @@
-import { get, post, put } from "./publicApiService";
-
+import { get, patch } from "./publicApiService";
+import { getAll, post, put } from "./privateApiService";
 const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT;
+
+export const patchSlides = () => {
+  return patch(ENDPOINT);
+};
 
 export const getSlides = () => {
   return get(ENDPOINT);
 };
 
-export const createSlides = () => {
-  return post(ENDPOINT);
+// The token is needed for this method
+export const getAllSlides = (id) => {
+  return getAll(`${ENDPOINT}/${id}`);
 };
 
-export const editSlides = () => {
-  return put(ENDPOINT);
+// The token is needed for this method
+export const postSlides = (data) => {
+  return post(ENDPOINT, data);
+};
+
+// The token is needed for this method
+export const putSlides = (id, data) => {
+  return put(`${ENDPOINT}/${id}`, data);
 };
