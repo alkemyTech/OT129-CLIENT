ab8b76ce89a90459ff0a7d76f95578fbf4397d4b
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-04 16:20:24 -0300

    Renamed container

--- src/Components/News/Details/NewsDetails.js
+++ src/Components/News/Details/NewsDetails.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const NewsDetails = ({ data }) => {
   return (
-    <div className="card">
+    <div className="container">
       <h1>{data.name}</h1>
       <div className="d-grid d-md-flex justify-content-md-center py-3">
         <img alt={data.name} className="mw-100" src={data.image} />
