2524b5db0a42012a29799139cb4413849192eee0
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-04 08:29:42 -0300

    docs: environment variables application

--- src/Services/HomeServices.js
+++ src/Services/HomeServices.js
@@ -1,13 +1,13 @@
 import { get } from "./publicApiService";
 import { post } from "./privateApiService";
 
-const ENDPOINT_PUBLIC = "slides";
-const ENDPOINT_PRIVATE = "organization";
+const ENDPOINT_SLIDES = process.env.REACT_APP_API_SLIDES_ENDPOINT;
+const ENDPOINT_ORGANIZATION = process.env.REACT_APP_API_ORGANIZATION_ENDPOINT;
 
 export const getSlides = () => {
-  return get(ENDPOINT_PUBLIC);
+  return get(ENDPOINT_SLIDES);
 };
 
 export const postOrganization = (data) => {
-  return post(ENDPOINT_PRIVATE, data);
+  return post(ENDPOINT_ORGANIZATION, data);
 };
