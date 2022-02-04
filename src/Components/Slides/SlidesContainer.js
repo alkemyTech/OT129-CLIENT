445deabd695bc325e874e84b3495d1e869623160
pablorosti <pablorostirollatenis@gmail.com>
2022-02-04 09:02:26 -0300

    fix: merged with dedvelopment branch

--- src/Components/Slides/SlidesContainer.js
+++ src/Components/Slides/SlidesContainer.js
@@ -12,8 +12,6 @@ const SlidesContainer = () => {
     const data = async () => {
       const slides = await getSlides();
 
-      console.log(slides);
-
       setSlides(slides.data.data);
     };
 
