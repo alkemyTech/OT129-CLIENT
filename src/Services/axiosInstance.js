e792ffd6eb31a7a4a819f5e377404871f692558e
Ivan Salas <ivanedusalas@gmail.com>
2022-02-04 17:55:34 -0300

    update branch

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const config = {
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  headers: {
-    Group: "01",
-  },
 };
 
 export default instance = axios.create(config);
