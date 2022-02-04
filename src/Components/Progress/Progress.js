2cb7f6a62324e575956d5715549bdb7bff8dcfd8
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-04 10:08:34 -0300

    Created component and styles

---
+++ src/Components/Progress/Progress.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../index.css";
+import "./Progress.css";
+
+const Progress = () => {
+  return <progress className="progress-linear" />;
+};
+
+export default Progress;
