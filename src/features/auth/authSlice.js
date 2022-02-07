e28afbd39f599709a59258dec12269b970382ddf
Ezequiel Galardi <eze3769@gmail.com>
2022-02-07 16:45:51 -0300

    fix: deleted unused import

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
 
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { login, register } from "../../Services/authServices";
