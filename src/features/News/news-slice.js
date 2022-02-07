54c6fc92c6b250720017a6464bb27efbc5f36662
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-07 10:37:10 -0300

    style: blank spaces correction

---
+++ src/features/News/news-slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const newsSlice = createSlice({
+    name: 'news',
+    initialState: {
+        newsList: [],
+    },
+    reducers: {
+        showList(state, action) {
+            state.newsList = action.payload.newsList;
+        },
+    }
+});
+
+export const newsActions = newsSlice.actions;
+
+export default newsSlice;
