7af1a14a0db837170826a0fcdeb18ad056f76f94
Ivan Salas <ivanedusalas@gmail.com>
2022-02-07 14:09:00 -0300

    feat: MembersService created, fix: Update enviroments variable

---
+++ src/Services/MembersService.js
@@ -0,0 +1,24 @@
+import { get } from "./publicApiService";
+import { post, put, remove } from "./privateApiService";
+
+const ENDPOINT = process.env.REACT_APP_API_MEMBERS_ENDPOINT;
+
+export const getMembers = () => {
+  return get(ENDPOINT);
+};
+
+export const getMemberByID = (id) => {
+  return get(ENDPOINT, id);
+};
+
+export const createMember = (data) => {
+  return post(ENDPOINT, data);
+};
+
+export const editMember = (data, id) => {
+  return put(ENDPOINT, data, id);
+};
+
+export const deleteMember = (id) => {
+  return remove(ENDPOINT, id);
+};
