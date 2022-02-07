70cf33ba26cc785edfbf90082fe8b4eac1706be4
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-07 11:08:48 -0300

    Fixed endpoint

--- src/Services/NewsService.js
+++ src/Services/NewsService.js
@@ -1,7 +1,7 @@
 import { get, post, patch } from "./publicApiService";
 import { remove } from "./privateApiService";
 
-const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT;
+const ENDPOINT = process.env.REACT_APP_API_NEWS_ENDPOINT;
 
 export const getNews = () => {
   return get(ENDPOINT);
