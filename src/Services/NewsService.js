687dd256ca8063d3fd2251c36ab9cb8726bd2a2b
Ana Galli <anaamgalli@gmail.com>
2022-02-07 15:35:21 -0300

    feat: Add last news get request

--- src/Services/NewsService.js
+++ src/Services/NewsService.js
@@ -1,10 +1,16 @@
 import { get, post, patch } from "./publicApiService";
 import { remove } from "./privateApiService";
 
-const ENDPOINT = process.env.REACT_APP_API_SLIDES_ENDPOINT;
+const ENDPOINT = process.env.REACT_APP_API_NEWS_ENDPOINT;
 
 export const getNews = () => {
   return get(ENDPOINT);
+};
+
+export const getLastNews = (entries) => {
+  const url = entries ? `${ENDPOINT}?limit=${entries}` : ENDPOINT;
+
+  return get(url);
 };
 
 export const createNews = () => {
