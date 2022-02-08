9d2e3c771f056a73f9ed0d03d1016952d9f7fffe
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-08 19:13:34 -0300

    Added reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import counterReducer from "../features/counter/counterSlice";
+import aboutReducer from "../features/About/aboutSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    about: aboutReducer,
   },
 });
