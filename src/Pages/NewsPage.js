64d9681865abca699ad7f861536b983f9ef9b8b5
Emiliano <82418581+emquiroga@users.noreply.github.com>
2022-02-08 18:44:13 -0300

    Ot129 100 comentarios (#79)
    * Fixed title prop
    
    * Created New Page
    
    * Updated route component
    
    * Created service
    
    * Created new component
    
    * Refactor and correct services
    
    * Moved
    
    * Created container and service
    
    * Added intersection dependency
    
    * Updated styles
    
    * Renamed container
    
    * Fixed key value
    
    * Working on intersection effect
    
    * Added new dependency
    
    * Fixed endpoint
    
    * Updated page component link
    
    * Added inView dep
    
    * Updated styles
    
    * Fixed bug at imports
    
    * Unnecessary prop validation
    
    * Added Progress documentation
    
    * Fixed height and width as props
    
    * Updated documentation
    
    * Added custom margin with props
    
    * Removed required from proptypes
    
    * FIxing skeleton
    
    * Updated styles
    
    * Created new skeleton component for comment
    
    * Added CommentSkeleton
    
    * Updated styles for mobile
    
    * Removed group for testing
    
    * Testing components
    
    * Added ID by params
    
    * Added group again

--- src/Pages/NewsPage.js
+++ src/Pages/NewsPage.js
@@ -1,26 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Title from "../Components/Titles/Titles";
 import NewsCards from "../Containers/NewsCards/NewsCards";
-import { getNews } from "../Services/NewsService";
 
 const NewsPage = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await getNews();
-
-      setData(response.data.data);
-    };
-
-    getData();
-  }, []);
-
   return (
     <>
-      <Title title={"Novedades"} />
-      <NewsCards data={data} />
+      <Title title="Novedades" />
+      <NewsCards />
     </>
   );
 };
