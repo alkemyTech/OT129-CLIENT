75d4dad321d7d23c207e6d9ba86306962c3ff90f
emquiroga <emiliano.quiroga093@gmail.com>
2022-02-08 10:19:56 -0300

    Removed required from proptypes

--- src/Components/Comment/Comment.js
+++ src/Components/Comment/Comment.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import "../../index.css";
 import "./Comment.css";
 
-const Comment = ({ data = {}, title }) => {
+const Comment = ({ data = [], title }) => {
   return (
     <div>
       <h2 className="comment-title" id="comenatrios">
@@ -12,21 +12,22 @@ const Comment = ({ data = {}, title }) => {
       </h2>
       <div className="comment-list-wrapper">
         <ul className="comment-list">
-          {data.map((comment, index) => {
-            return (
-              <li key={`${comment.user_id}${index}`} className="comment-item">
-                <div className="comment-item-wrapper">
-                  <div className="comment-user-image">
-                    <img alt="User image" src={comment.image} />
+          {data.length > 0 &&
+            data.map((comment, index) => {
+              return (
+                <li key={`${comment.user_id}${index}`} className="comment-item">
+                  <div className="comment-item-wrapper">
+                    <div className="comment-user-image">
+                      <img alt="User image" src={comment.image} />
+                    </div>
+                    <div className="comment-item-content">
+                      <h4 className="comment-content-user">{comment.user_id ?? "Custom User"}</h4>
+                      <p className="comment-content-text">{comment.text}</p>
+                    </div>
                   </div>
-                  <div className="comment-item-content">
-                    <h4 className="comment-content-user">{comment.user_id ?? "Custom User"}</h4>
-                    <p className="comment-content-text">{comment.text}</p>
-                  </div>
-                </div>
-              </li>
-            );
-          })}
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
@@ -34,12 +35,14 @@ const Comment = ({ data = {}, title }) => {
 };
 
 Comment.propTypes = {
-  data: PropTypes.shape({
-    user_id: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-  }).isRequired,
-  title: PropTypes.string.isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      user_id: PropTypes.number,
+      image: PropTypes.string,
+      text: PropTypes.string,
+    })
+  ),
+  title: PropTypes.string,
 };
 
 export default Comment;
