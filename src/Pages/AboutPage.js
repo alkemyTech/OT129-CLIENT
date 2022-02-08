ecbd49afff2243f42bd5f9319b13da1c6a3bfb79
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-08 19:15:35 -0300

    Added containers

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-import About from "../Components/About/About";
+import Titles from "../Components/Titles/Titles";
+import AboutInfoContainer from "../Containers/About/AboutInfoContainer";
+import MemberCardContainer from "../Containers/Members/MemberCardContainer";
 
 const AboutPage = () => {
-  return <About />;
+  return (
+    <>
+      <Titles title="Nosotros" />
+      <AboutInfoContainer />
+      <MemberCardContainer />
+    </>
+  );
 };
 
 export default AboutPage;
