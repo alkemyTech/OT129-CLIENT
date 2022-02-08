e68649b815088c3951b6937ddd79079a8b35132a
Ivan Salas <ivanedusalas@gmail.com>
2022-02-07 22:56:28 -0300

    fix : merge conflict resolution

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const config = {
   baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    Group: "01",
+  },
 };
 
 export default instance = axios.create(config);
