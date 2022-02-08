70a63fc6a603d8ce7f248ada443f317d1e5b509b
emquiroga <emiliano.quiroga093@gmail.com>
2022-02-08 09:11:14 -0300

    Fixed height and width as props

--- src/Components/Progress/Progress.js
+++ src/Components/Progress/Progress.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "../../index.css";
 import "./Progress.css";
 
-const Progress = () => {
+const Progress = ({ height, width }) => {
   return (
     <div className="progress-container">
-      <progress className="progress-linear" />
+      <progress className="progress-linear" style={{ height: `${height}`, width: `${width}` }} />
     </div>
   );
+};
+
+Progress.propTypes = {
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
 };
 
 export default Progress;
