e752f90f5579f2f779690a9d3719ae339e6adf66
Ivan Salas <ivanedusalas@gmail.com>
2022-02-09 17:49:45 -0300

    feat: users reducer added to the store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import newsReducer from "../features/News/news-slice";
 import slidesReducer from "../features/slides/slidesSlice";
+import usersReducer from "../features/user/users-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     news: newsReducer,
     slides: slidesReducer,
+    users: usersReducer,
   },
 });
 
