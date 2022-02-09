81cfd52cfa12e05d11cb1c4305b7249e7b74c51e
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-02-09 17:03:55 -0300

    feat: add categories reducer into store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "../features/auth/authSlice";
+import categoriesReducer from "../features/Categories/categoriesSlice";
 import newsReducer from "../features/News/news-slice";
 import slidesReducer from "../features/slides/slidesSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    categories: categoriesReducer,
     news: newsReducer,
     slides: slidesReducer,
   },
