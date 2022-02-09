fe8e31c248575c238b35b3d983868fcbdfe0ae05
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-09 14:02:42 -0300

    docs: added slogan

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import logoNgo from "../../assets/ngologo.png";
 import logoCampaign from "../../assets/Toys/toyscampaign.png";
+
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.headerCtn}>
+    <div className="container">
       <div className={styles.headerInfo}>
         <img alt="Logo de la campaña" className={styles.logoCampaign} src={logoCampaign} />
+        <p className={styles.slogan}>
+          JUGUETES POR <span className={styles.spanbreak}>MÁS SONRISAS</span>
+        </p>
         <img alt="Logo de la ONG" className={styles.logoNgo} src={logoNgo} />
       </div>
     </div>
