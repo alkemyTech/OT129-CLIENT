eab0795d073d0bef55c0aa44f46e589b56d2ee92
Ivan Salas <ivanedusalas@gmail.com>
2022-02-09 17:57:25 -0300

    fix: refactoring of http requests, use of redux

--- src/Containers/Users/UsersListContainer.js
+++ src/Containers/Users/UsersListContainer.js
@@ -1,26 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { fetchUsers, selectorUsers } from "../../features/user/users-slice";
 import BackUsersList from "../../Components/Users/BackUsersList";
 import TitleNav from "../../Components/TitleNav/TitleNav";
-import { getUsers } from "../../Services/UsersService";
 
 const UsersListContainer = () => {
-  const [dataUsers, setDataUsers] = useState([]);
+  const { users } = useSelector(selectorUsers);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const data = async () => {
-      const users = await getUsers();
-
-      setDataUsers(users.data.data);
-    };
-
-    data();
-  }, []);
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   return (
     <div className="container mt-5">
       <TitleNav link="/backoffice/users/create" linkTitle="Crear" title="Users" />
-      <BackUsersList data={dataUsers} />
+      <BackUsersList data={users} />
     </div>
   );
 };
