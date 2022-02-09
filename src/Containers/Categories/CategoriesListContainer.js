78a9eb3cfbd657f8387d59d179c93ee4759812c4
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-02-09 17:49:56 -0300

    refactor: use dispatch to get all categories

--- src/Containers/Categories/CategoriesListContainer.js
+++ src/Containers/Categories/CategoriesListContainer.js
@@ -1,26 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import TitleNav from "../../Components/TitleNav/TitleNav";
+import { fetchCategories, selectorCategories } from "../../features/Categories/categoriesSlice";
 import CategoriesList from "../../Components/Categories/CategoriesList";
-import { getCategories } from "../../Services/CategoriesService";
 
 const CategoriesListContainer = () => {
-  const [dataCategories, setDataCategories] = useState([]);
+  const { categories } = useSelector(selectorCategories);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const data = async () => {
-      const categories = await getCategories();
-
-      setDataCategories(categories.data.data);
-    };
-
-    data();
-  }, []);
+    dispatch(fetchCategories());
+  }, [dispatch]);
 
   return (
     <div className="container mt-5">
       <TitleNav link="/backoffice/categories/create" linkTitle="Crear" title="Categorías" />
-      <CategoriesList data={dataCategories} />
+      <CategoriesList data={categories} />
     </div>
   );
 };
