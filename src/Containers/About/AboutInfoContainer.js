51538afcfd6a3a40823b9dd3d2d359b5b28446d8
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-09 13:03:08 -0300

    Added dispatchs and error handling

--- src/Containers/About/AboutInfoContainer.js
+++ src/Containers/About/AboutInfoContainer.js
@@ -3,12 +3,33 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 import AboutInfo from "../../Components/About/AboutInfo";
-import { fetchOrganization, selectorOrganization } from "../../features/About/aboutSlice";
+import {
+  fetchOrganization,
+  selectorOrganization,
+} from "../../features/Organization/organizationSlice";
+import Spinner from "../../Components/Spinner/Spinner";
+import { alerts } from "../../utils/alerts";
 
 const AboutInfoContainer = () => {
   const dispatch = useDispatch();
 
-  const { long_description } = useSelector(selectorOrganization);
+  const {
+    organization: { long_description },
+  } = useSelector(selectorOrganization);
+
+  const { status } = useSelector(selectorOrganization);
+
+  const handleErrors = () => {
+    if (status === "loading") {
+      return (
+        <div className="container">
+          <Spinner />
+        </div>
+      );
+    } else if (status === "failed") {
+      alerts("Lo sentimos! La infrmación no se encuentra disponible.", "error");
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchOrganization());
@@ -18,6 +39,7 @@ const AboutInfoContainer = () => {
     <div className="container mt-5 text-center">
       <h3 className="text-uppercase mb-4">Sobre la ONG</h3>
       {long_description && <AboutInfo description={long_description} />}
+      {handleErrors()}
     </div>
   );
 };
--- src/Containers/Members/MemberCardContainer.js
+++ src/Containers/Members/MemberCardContainer.js
@@ -1,14 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
+import MemberCard from "../../Components/About/MemberCard";
+import Spinner from "../../Components/Spinner/Spinner";
+import { alerts } from "../../utils/alerts";
 import { fetchMembers, selectorMembers } from "../../features/Members/membersSlice";
+
+import "./MemberCardContainer.css";
 
 const MemberCardContainer = () => {
   const dispatch = useDispatch();
-  const members = useSelector(selectorMembers);
+  const { members } = useSelector(selectorMembers);
+  const { status } = useSelector(selectorMembers);
 
-  console.log(members);
+  const handleErrors = () => {
+    if (status === "loading") {
+      return (
+        <div className="container">
+          <Spinner />
+        </div>
+      );
+    } else if (status === "failed") {
+      alerts("Lo sentimos! La infrmación no se encuentra disponible.", "error");
+    }
+  };
 
   useEffect(() => {
     dispatch(fetchMembers());
@@ -17,6 +33,10 @@ const MemberCardContainer = () => {
   return (
     <div className="container my-5">
       <h3 className="text-center mb-5">MIEMBROS</h3>
+      <div className="member-card-container">
+        {members && members.map((member) => <MemberCard key={member.id} {...member} />)}
+        {handleErrors()}
+      </div>
     </div>
   );
 };
