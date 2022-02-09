bf423341658d01e91edd138de741e6a1df29880e
Ana Galli <anaamgalli@gmail.com>
2022-02-09 16:29:22 -0300

    feat: add activities reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import newsReducer from "../features/News/news-slice";
 import slidesReducer from "../features/slides/slidesSlice";
+import activitiesReducer from "../features/Activities/activitiesSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     news: newsReducer,
     slides: slidesReducer,
+    activities: activitiesReducer,
   },
 });
 
