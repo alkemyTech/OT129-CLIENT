01ba311b7ef17d794c86dee77e4b2e766a0898f0
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-09 13:02:21 -0300

    added organization and members reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import aboutReducer from "../features/About/aboutSlice";
 import authReducer from "../features/auth/authSlice";
 import newsReducer from "../features/News/news-slice";
 import slidesReducer from "../features/slides/slidesSlice";
+import organizationReducer from "../features/Organization/organizationSlice";
+import membersReducer from "../features/Members/membersSlice";
 
 const store = configureStore({
   reducer: {
-    about: aboutReducer,
     auth: authReducer,
     news: newsReducer,
     slides: slidesReducer,
+    organization: organizationReducer,
+    members: membersReducer,
   },
 });
 
