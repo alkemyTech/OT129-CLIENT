50a763978df312580375c59534433a496ef2a3c2
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-09 13:02:25 -0300

    feat: added Toys campaign header

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Header = () => <div />;
+import logoNgo from "../../assets/ngologo.png";
+import logoCampaign from "../../assets/Toys/toyscampaign.png";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.headerCtn}>
+      <div className={styles.headerInfo}>
+        <img alt="Logo de la campaña" className={styles.logoCampaign} src={logoCampaign} />
+        <img alt="Logo de la ONG" className={styles.logoNgo} src={logoNgo} />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
