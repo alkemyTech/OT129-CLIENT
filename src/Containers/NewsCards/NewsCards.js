ef11e8cf5b1e82ff49097765a7080462d626333a
Emiliano <82418581+emquiroga@users.noreply.github.com>
2022-02-10 19:23:37 -0300

    Redux status file (#96)
    * Created new file
    
    * Imported constant
    
    * Updated status string
    
    * Added thunk services and status handler

--- src/Containers/NewsCards/NewsCards.js
+++ src/Containers/NewsCards/NewsCards.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import Card from "../../Components/Card/Card";
-import { getNews } from "../../Services/NewsService";
+import StatusHandler from "../../Components/StatusHandler/StatusHandler";
+import { fetchNews, selectorNews } from "../../features/News/news-slice";
 
 const NewsCards = () => {
-  const [news, setNews] = useState([]);
+  const dispatch = useDispatch();
+  const { news, status } = useSelector(selectorNews);
 
   useEffect(() => {
-    getNews()
-      .then((response) => {
-        setNews(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    dispatch(fetchNews());
+  }, [dispatch]);
 
   return (
     <div className="container my-5 d-grid gap-3">
       <div className="row">
-        {news.map((el) => (
-          <div key={el.id} className="col">
-            <Card
-              description={el.content}
-              id={el.id}
-              image={el.image}
-              title={el.name}
-              url={`./novedades`}
-            />
-          </div>
-        ))}
+        <StatusHandler status={status} />
+        {news &&
+          news.map((el) => (
+            <div key={el.id} className="col">
+              <Card
+                description={el.content}
+                id={el.id}
+                image={el.image}
+                title={el.name}
+                url={`./novedades`}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
