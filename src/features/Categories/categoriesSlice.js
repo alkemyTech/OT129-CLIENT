c465f2ece28ac70c6625c1bcf7a3fe9be6ea9b6e
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-02-10 14:40:21 -0300

    add create, getbyid & edit thunks and reducers

--- src/features/Categories/categoriesSlice.js
+++ src/features/Categories/categoriesSlice.js
@@ -1,11 +1,37 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getCategories, deleteCategory } from "../../Services/CategoriesService";
+import {
+  getCategories,
+  deleteCategory,
+  getCategoryById,
+  createCategory,
+  editCategory,
+} from "../../Services/CategoriesService";
 
 export const fetchCategories = createAsyncThunk("categories/get", async () => {
   const {
     data: { data },
   } = await getCategories();
+
+  return data;
+});
+
+export const fetchCategoryById = createAsyncThunk("category/get", async (id) => {
+  const {
+    data: { data },
+  } = await getCategoryById(id);
+
+  return data;
+});
+
+export const newCategory = createAsyncThunk("category/post", async (data) => {
+  await createCategory(data);
+});
+
+export const putCategory = createAsyncThunk("category/put", async (formData) => {
+  const {
+    data: { data },
+  } = await editCategory(formData.data, formData.id);
 
   return data;
 });
@@ -20,6 +46,7 @@ const categoriesSlice = createSlice({
   name: "categories",
   initialState: {
     categories: [],
+    category: {},
     status: null,
   },
   extraReducers: {
@@ -31,6 +58,35 @@ const categoriesSlice = createSlice({
       state.categories = action.payload;
     },
     [fetchCategories.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [fetchCategoryById.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchCategoryById.fulfilled]: (state, { payload }) => {
+      state.status = "success";
+      state.category = payload;
+    },
+    [fetchCategoryById.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [newCategory.pending]: (state) => {
+      state.status = "loading";
+    },
+    [newCategory.fulfilled]: (state) => {
+      state.status = "success";
+    },
+    [newCategory.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [putCategory.pending]: (state) => {
+      state.status = "loading";
+    },
+    [putCategory.fulfilled]: (state, { payload }) => {
+      state.status = "success";
+      state.category = payload;
+    },
+    [putCategory.rejected]: (state) => {
       state.status = "failed";
     },
     [removeCategory.pending]: (state) => {
