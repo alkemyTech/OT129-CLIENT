1fa77a828e52203a063de4444f2eb78f5a6d3911
Ivan Salas <ivanedusalas@gmail.com>
2022-02-10 11:18:21 -0300

    feat: the sidebar component is added with its respective backoffice links

--- src/Components/BackNavBar/BackNavBar.js
+++ src/Components/BackNavBar/BackNavBar.js
@@ -1,11 +1,10 @@
 import React from "react";
-// eslint-disable-next-line no-unused-vars
-import { Offcanvas } from "bootstrap";
+import { offcanvas } from "bootstrap";
 import { Link } from "react-router-dom";
 
 import logo from "./logo-modified.png";
 
-function BackNavBar() {
+const BackNavBar = () => {
   return (
     <nav className="navbar navbar-light bg-white">
       <div className="container-fluid">
@@ -38,46 +37,49 @@ function BackNavBar() {
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li className="nav-item">
-                <a aria-current="page" className="nav-link active" href="#">
+                <Link className="nav-link" to="/backoffice/organization">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
+                <Link className="nav-link" to="/backoffice/organization">
+                  Organización
+                </Link>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  aria-expanded="false"
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  href="#"
-                  id="offcanvasNavbarDropdown"
-                  role="button"
-                >
-                  Dropdown
-                </a>
-                <ul aria-labelledby="offcanvasNavbarDropdown" className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/activities">
+                  Actividades
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/news">
+                  Novedades
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/categories">
+                  Categories
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/slides">
+                  Slides
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/members">
+                  Miembros
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/users">
+                  Usuarios
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/backoffice/testimonies">
+                  Testimonios
+                </Link>
               </li>
             </ul>
           </div>
@@ -88,6 +90,6 @@ function BackNavBar() {
       </div>
     </nav>
   );
-}
+};
 
 export default BackNavBar;
