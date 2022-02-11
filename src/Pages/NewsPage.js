1ca4ba04e38de17536f4c85a9318e76f0dfe2b50
Antony Fagundez <jhonfagundez@gmail.com>
2022-02-10 21:37:02 -0300

    chore: remove unnecesary vars

--- src/Pages/NewsPage.js
+++ src/Pages/NewsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Title from "../Components/Titles/Titles";
 import NewsCards from "../Containers/NewsCards/NewsCards";
