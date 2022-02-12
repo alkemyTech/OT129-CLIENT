6e0954b34ad401768695f70856edd8f8b0b1ce6c
Ivan Salas <ivanedusalas@gmail.com>
2022-02-12 10:51:37 -0300

    feat: http requests are extended

--- src/features/user/users-slice.js
+++ src/features/user/users-slice.js
@@ -1,6 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getUsers } from "../../Services/UsersService";
+import {
+  createUsers,
+  editUsers,
+  getUsers,
+  getUsersByID,
+  deleteUsers,
+} from "../../Services/UsersService";
 export const fetchUsers = createAsyncThunk("users/get", async () => {
   const {
     data: { data },
@@ -9,10 +15,33 @@ export const fetchUsers = createAsyncThunk("users/get", async () => {
   return data;
 });
 
+export const fetchUsersById = createAsyncThunk("user/get", async (id) => {
+  const {
+    data: { data },
+  } = await getUsersByID(id);
+
+  return data;
+});
+
+export const newUser = createAsyncThunk("user/post", async (data) => {
+  await createUsers(data);
+});
+
+export const putUser = createAsyncThunk("user/put", async (formData) => {
+  await editUsers(formData.data, formData.id);
+});
+
+export const removeUser = createAsyncThunk("users/delete", async (id) => {
+  await deleteUsers();
+
+  return id;
+});
+
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     users: [],
+    user: {},
     status: null,
   },
   extraReducers: {
@@ -24,6 +53,46 @@ const usersSlice = createSlice({
       state.users = action.payload;
     },
     [fetchUsers.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [fetchUsersById.pending]: (state) => {
+      state.status = "loading";
+    },
+    [fetchUsersById.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.user = action.payload;
+    },
+    [fetchUsersById.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [newUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [newUser.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.users = action.payload;
+    },
+    [newUser.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [putUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [putUser.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.users = action.payload;
+    },
+    [putUser.rejected]: (state) => {
+      state.status = "failed";
+    },
+    [removeUser.pending]: (state) => {
+      state.status = "loading";
+    },
+    [removeUser.fulfilled]: (state, action) => {
+      state.status = "success";
+      state.users = action.payload;
+    },
+    [removeUser.rejected]: (state) => {
       state.status = "failed";
     },
   },
