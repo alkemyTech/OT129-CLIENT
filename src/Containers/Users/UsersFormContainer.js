58f290deec3ba483ebc0f41bbba875bf292536ff
Ivan Salas <ivanedusalas@gmail.com>
2022-02-12 10:52:26 -0300

    feat: http requests are extended to containers

--- src/Containers/Users/UsersFormContainer.js
+++ src/Containers/Users/UsersFormContainer.js
@@ -1,24 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import UsersForm from "../../Components/Users/UsersForm";
-import { getUsersByID } from "../../Services/UsersService";
+import { newUser, selectorUsers, fetchUsersById, putUser } from "../../features/user/users-slice";
 
 const UsersFormContainer = () => {
   const { id } = useParams();
-  const [users, setUsers] = useState({});
+  const { user } = useSelector(selectorUsers);
+  const dispatch = useDispatch();
+
+  const handleSub = (data) => {
+    if (!id) {
+      dispatch(newUser(data))
+        .then(() => {
+          alerts(`Categoría creada correctamente`, "success");
+        })
+        .catch(() => {
+          alerts("Ups! ocurrió un error inesperado al crear la categoría", "error");
+        });
+    } else {
+      confirmAlerts(
+        "¿Estás seguro?",
+        `Se editará la categoría id: ${user.id}`,
+        function (response) {
+          if (response) {
+            dispatch(putUser({ data, id: user.id }))
+              .then(() => {
+                alerts(`La categoría id: ${user.id} se editó correctamente`, "success");
+              })
+              .catch(() => {
+                alerts(`Ocurrió un error al editar la categoría id: ${user.id} `, "error");
+              });
+          }
+        }
+      );
+    }
+  };
 
   useEffect(() => {
     if (id) {
-      getUsersByID(id).then((result) => {
-        const response = result.data.data;
-
-        setUsers(response);
-      });
+      dispatch(fetchUsersById(id));
     }
   }, [id]);
 
-  return <UsersForm users={users} />;
+  return <UsersForm handleSubmit={handleSub} users={user} />;
 };
 
 export default UsersFormContainer;
--- src/Containers/Users/UsersListContainer.js
+++ src/Containers/Users/UsersListContainer.js
@@ -1,13 +1,33 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { fetchUsers, selectorUsers } from "../../features/user/users-slice";
+import { fetchUsers, removeUser, selectorUsers } from "../../features/user/users-slice";
 import BackUsersList from "../../Components/Users/BackUsersList";
 import TitleNav from "../../Components/TitleNav/TitleNav";
+import Spinner from "../../Components/Spinner/Spinner";
+import { confirmAlerts } from "../../utils/alerts";
 
 const UsersListContainer = () => {
-  const { users } = useSelector(selectorUsers);
+  const { users, status } = useSelector(selectorUsers);
   const dispatch = useDispatch();
+
+  const onDelete = (id) => {
+    confirmAlerts(
+      ";Estas Seguro?",
+      `La categoría id: ${id} se eliminará permanentemente`,
+      function (response) {
+        if (response) {
+          dispatch(removeUser(id))
+            .then(() => {
+              alerts(`La categoría id: ${id} se eliminó correctamente`, "success");
+            })
+            .catch(() => {
+              alerts(`Ocurrió un error al eliminar la categoría id: ${id} `, "error");
+            });
+        }
+      }
+    );
+  };
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -16,7 +36,8 @@ const UsersListContainer = () => {
   return (
     <div className="container mt-5">
       <TitleNav link="/backoffice/users/create" linkTitle="Crear" title="Users" />
-      <BackUsersList data={users} />
+      <BackUsersList data={users} onDelete={onDelete} />
+      {status === "loading" ? <Spinner /> : null}
     </div>
   );
 };
