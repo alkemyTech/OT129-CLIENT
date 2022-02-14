6c220796ab1e5aeacffca7c192a757a46b581c4e
Your Name <you@example.com>
2022-02-14 12:41:38 -0300

    added School Header.js

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-const Header = () => <div />;
+import logoNgo from "../../assets/onglogo.png";
+import logoCampaign from "../../assets/School/schoolcampaign.png";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className={styles.headerInfo}>
+        <img alt="Logo de la campaña" className={styles.logoCampaign} src={logoCampaign} />
+        <p className={styles.slogan}>
+          JUNTOS EN LA <span className={styles.spanbreak}>VUELTA AL COLE</span>
+        </p>
+        <img alt="Logo de la ONG" className={styles.logoNgo} src={logoNgo} />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
