2e613133a1a0b53da3227a598a3101ebac1f2159
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-15 10:30:27 -0300

    Added redux service and status handler

--- src/Containers/Members/MembersListContainer.js
+++ src/Containers/Members/MembersListContainer.js
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
+import { fetchMembers, selectorMembers } from "../../features/Members/membersSlice";
 import MembersList from "../../Components/Members/MembersList";
 import TitleNav from "../../Components/TitleNav/TitleNav";
-import { getMembers } from "../../Services/MembersService";
+import StatusHandler from "../../Components/StatusHandler/StatusHandler";
 
 const MembersListContainer = () => {
-  const [dataMembers, setDataMembers] = useState([]);
+  const dispatch = useDispatch();
+  const { members, status } = useSelector(selectorMembers);
 
   useEffect(() => {
-    const data = async () => {
-      const members = await getMembers();
-
-      setDataMembers(members.data.data);
-    };
-
-    data();
-  }, []);
+    dispatch(fetchMembers());
+  }, [dispatch]);
 
   return (
     <div className="container mt-5">
       <TitleNav link="/backoffice/members/create" linkTitle="Crear" title="Miembros" />
-      <MembersList data={dataMembers} />
+      <MembersList data={members} />
+      <StatusHandler status={status} />
     </div>
   );
 };
