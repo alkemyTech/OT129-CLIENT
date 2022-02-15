1b7fdd3fe22daf15ae66724ba2355fe940f18807
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-15 10:30:15 -0300

    Created new methods, updated extra reducers

--- src/features/Members/membersSlice.js
+++ src/features/Members/membersSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-import { getMembers } from "../../Services/MembersService";
+import { getMembers, getMemberByID, createMember, editMember } from "../../Services/MembersService";
 import { STATUS } from "../../constants";
 
 export const fetchMembers = createAsyncThunk("members/get", async () => {
@@ -9,6 +9,22 @@ export const fetchMembers = createAsyncThunk("members/get", async () => {
   } = await getMembers();
 
   return data;
+});
+
+export const fetchMemberById = createAsyncThunk("member/get", async (id) => {
+  const {
+    data: { data },
+  } = await getMemberByID(id);
+
+  return data;
+});
+
+export const newMember = createAsyncThunk("member/post", async (data) => {
+  await createMember(data);
+});
+
+export const putMember = createAsyncThunk("member/put", async (data) => {
+  await editMember(data, data.id);
 });
 
 const memberSlice = createSlice({
@@ -26,6 +42,36 @@ const memberSlice = createSlice({
       state.members = action.payload;
     },
     [fetchMembers.rejected]: (state) => {
+      state.status = STATUS.FAILED;
+    },
+    [fetchMemberById.pending]: (state) => {
+      state.status = STATUS.PENDING;
+    },
+    [fetchMemberById.fulfilled]: (state, action) => {
+      state.status = STATUS.SUCCESSFUL;
+      state.members = action.payload;
+    },
+    [fetchMemberById.rejected]: (state) => {
+      state.status = STATUS.FAILED;
+    },
+    [newMember.pending]: (state) => {
+      state.status = STATUS.PENDING;
+    },
+    [newMember.fulfilled]: (state, action) => {
+      state.status = STATUS.SUCCESSFUL;
+      state.members = action.payload;
+    },
+    [newMember.rejected]: (state) => {
+      state.status = STATUS.FAILED;
+    },
+    [putMember.pending]: (state) => {
+      state.status = STATUS.PENDING;
+    },
+    [putMember.fulfilled]: (state, action) => {
+      state.status = STATUS.SUCCESSFUL;
+      state.members = action.payload;
+    },
+    [putMember.rejected]: (state) => {
       state.status = STATUS.FAILED;
     },
   },
