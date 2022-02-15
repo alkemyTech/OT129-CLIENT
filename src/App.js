a60120f8b2d32f8c37c8e85a06da9f0ff3b0bb6e
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-15 17:47:20 -0300

    docs: application of privateroutes to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import LoginForm from "./Components/Auth/LoginForm";
-import ActivitiesListContainer from "./Containers/Activities/ActivitiesListContainer";
-import ActivitiesFormContainer from "./Containers/Activities/ActivitiesFormContainer/ActivitiesFormContainer";
 import ActivitiesPage from "./Pages/ActivitiesPage";
 import ActivitiesIdPage from "./Pages/ActivitiesIdPage";
 import NewsPage from "./Pages/NewsPage";
@@ -13,34 +12,25 @@ import ProjectsForm from "./Components/Projects/ProjectsForm";
 import AboutPage from "./Pages/AboutPage";
 import ContactPage from "./Pages/ContactPage";
 import Donation from "./Components/Donations/Donation";
-import EditOrganization from "./Containers/EditOrganization/EditOrganization";
 import HomePage from "./Pages/HomePage";
-import HomeForm from "./Components/Home/HomeForm";
-import NewsList from "./Components/News/NewsList";
-import CategoriesListContainer from "./Containers/Categories/CategoriesListContainer";
-import CategoriesFormContainer from "./Containers/Categories/CategoriesFormContainer";
-import MembersListContainer from "./Containers/Members/MembersListContainer";
-import MembersFormContainer from "./Containers/Members/MembersFormContainer";
-import NewsContainer from "./Containers/News/NewsContainer";
-import OrganizationContainer from "./Containers/Organization/OrganizationContainer";
-import SlidesContainer from "./Components/Slides/SlidesContainer";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import UsersListContainer from "./Containers/Users/UsersListContainer";
-import UsersFormContainer from "./Containers/Users/UsersFormContainer";
 import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
 import ThankYou from "./Components/Donations/ThankYou";
 import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import Backoffice from "./Containers/Backoffice/Backoffice";
 import RegisterForm from "./Components/Auth/RegisterForm";
 import Error404 from "./Pages/Error404Page";
 import Route from "./Components/Route";
 import PrivateRoute from "./Components/Route/PrivateRoute";
+import ProtectedRoutes from "./Components/Route/ProtectedRoutes";
+import { getToken, selectAuth } from "./features/auth/authSlice";
+import { isLogin } from "./utils/isLogin";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/js/dist/offcanvas";
 import "./App.css";
 
 function App() {
+  const login = isLogin();
+
   return (
     <>
       <Router>
@@ -62,52 +52,9 @@ function App() {
             <Route exact component={TestimonialForm} path="/testimonials/create" />
             <Route exact component={ProjectsForm} path="/projects/create" />
             <Route exact component={LoginForm} path="/login" />
-            <PrivateRoute exact component={Backoffice} path="/backoffice" />
-            <PrivateRoute exact component={HomeForm} path="/backoffice/home" />
-
-            <PrivateRoute exact component={ActivitiesListContainer} path="/backoffice/activities" />
-            <PrivateRoute
-              exact
-              component={ActivitiesFormContainer}
-              path="/backoffice/activities/create"
-            />
-            <PrivateRoute
-              exact
-              component={ActivitiesFormContainer}
-              path="/backoffice/activities/:id"
-            />
-            <PrivateRoute exact component={NewsList} path="/backoffice/news" />
-            <PrivateRoute exact component={NewsContainer} path="/backoffice/news/create" />
-            <PrivateRoute exact component={NewsContainer} path="/backoffice/news/:id" />
-            <PrivateRoute exact component={CategoriesListContainer} path="/backoffice/categories" />
-            <PrivateRoute
-              exact
-              component={CategoriesFormContainer}
-              path="/backoffice/categories/create"
-            />
-            <PrivateRoute
-              exact
-              component={CategoriesFormContainer}
-              path="/backoffice/categories/:id"
-            />
-            <PrivateRoute exact component={SlidesContainer} path="/backoffice/slides" />
-            <PrivateRoute exact component={SlidesForm} path="/backoffice/slides/create" />
-            <PrivateRoute exact component={UsersListContainer} path="/backoffice/users" />
-            <PrivateRoute exact component={UsersFormContainer} path="/backoffice/users/create" />
-            <PrivateRoute
-              exact
-              component={UsersFormContainer}
-              path="/backoffice/users/create/:id"
-            />
-            <PrivateRoute exact component={MembersListContainer} path="/backoffice/members" />
-            <PrivateRoute
-              exact
-              component={MembersFormContainer}
-              path="/backoffice/members/create"
-            />
-            <PrivateRoute exact component={MembersFormContainer} path="/backoffice/members/:id" />
-            <PrivateRoute exact component={OrganizationContainer} path="/backoffice/organization" />
-            <PrivateRoute exact component={EditOrganization} path="/backoffice/organization/edit" />
+            <PrivateRoute path="/">
+              <ProtectedRoutes />
+            </PrivateRoute>
             <Route component={Error404} path="*" />
           </Switch>
         </div>
