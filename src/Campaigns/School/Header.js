4e3adf8e2d87e7c1a7ba3efefab00d3d418f4195
Ivan Salas <ivanedusalas@gmail.com>
2022-02-17 19:12:05 -0300

    solved margin issues

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -7,7 +7,7 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className="container">
+    <div className="container my-2 p-2">
       <div className={styles.headerInfo}>
         <img alt="Logo de la campaña" className={styles.logoEscolar} src={LogoEscolar} />
         <p className={styles.slogan}>
