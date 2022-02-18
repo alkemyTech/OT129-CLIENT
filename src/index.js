a9956dbfaf57c769656fef336b415076b85e4675
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-18 19:14:46 -0300

    fix: index eslint fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 import App from "./App";
 import store from "./app/store";
@@ -15,10 +15,10 @@ let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App/>
-      </PersistGate>
-    </Provider>,
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
 
