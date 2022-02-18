e66762ef3da564f5c5bcf0fee5c3cef719709a31
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-18 18:46:54 -0300

    feat: new persist store

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {combineReducers} from "redux"; 
+import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+import storage from 'redux-persist/lib/storage'
+
 
 import authReducer from "../features/auth/authSlice";
 import categoriesReducer from "../features/Categories/categoriesSlice";
@@ -9,8 +14,7 @@ import organizationReducer from "../features/Organization/organizationSlice";
 import membersReducer from "../features/Members/membersSlice";
 import usersReducer from "../features/user/users-slice";
 
-const store = configureStore({
-  reducer: {
+const reducers = combineReducers({
     auth: authReducer,
     categories: categoriesReducer,
     news: newsReducer,
@@ -19,7 +23,18 @@ const store = configureStore({
     organization: organizationReducer,
     members: membersReducer,
     users: usersReducer,
-  },
+})
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
 });
 
 export default store;
