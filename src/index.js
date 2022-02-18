f42aa2af28061e7a7f4e6ef149c98387fbf8e556
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-18 19:20:02 -0300

    fix: index eslint error fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 import App from "./App";
 import store from "./app/store";
