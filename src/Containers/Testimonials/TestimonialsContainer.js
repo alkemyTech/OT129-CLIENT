8f87102d406a0567a393892b3ac0aee56451b3d9
Ana Galli <anaamgalli@gmail.com>
2022-02-18 15:43:28 -0300

    feat: create testominals page and components

---
+++ src/Containers/Testimonials/TestimonialsContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Testimonials from "../../Components/Testimonials/Testimonials/Testimonials";
+
+const TestimonialsContainer = () => {
+  return (
+    <div className="container">
+      <Testimonials />
+    </div>
+  );
+};
+
+export default TestimonialsContainer;
