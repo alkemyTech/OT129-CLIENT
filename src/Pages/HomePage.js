5d0ebd1b34f2863597289c13939ad15df54e2c22
Ana Galli <anaamgalli@gmail.com>
2022-02-18 15:45:17 -0300

    refactor: homePage components

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,26 +1,18 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 
 import HomeSlidesContainer from "../Containers/HomeSlide/HomeSlidesContainer";
-import HomeNewsCards from "../Containers/HomeCards/HomeNewsCards";
-import { fetchOrganization } from "../features/Organization/organizationSlice";
+import HomeOrganizationText from "../Components/Home/HomeOrganizationText/HomeOrganizationText";
+import HomeNewsCards from "../Components/Home/HomeNewsCards/HomeNewsCards";
+import HomeTestimonialsCards from "../Components/Home/HomeTestimonialsCards/HomeTestimonialsCards";
 
 const HomePage = () => {
-  const cardNews = "Cards Desde la API Novedades";
-  const welcomeText = "Texto de Bienvenida";
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchOrganization());
-  });
-
   return (
-    <div className="container">
-      <h1 className="text-center mt-3">Home Page</h1>
+    <>
       <HomeSlidesContainer />
-      <h2> {welcomeText}</h2>
-      <HomeNewsCards card={cardNews} />
-    </div>
+      <HomeOrganizationText />
+      <HomeNewsCards />
+      <HomeTestimonialsCards />
+    </>
   );
 };
 
