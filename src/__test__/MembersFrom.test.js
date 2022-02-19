c7c623fb6e20078c6dc989bf3dad4f322310bd15
malks14 <73920001+malks14@users.noreply.github.com>
2022-02-19 13:26:28 -0300

    feat: added memberform test

---
+++ src/__test__/MembersFrom.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import MembersForm from "../Components/Members/MembersForm";
+
+describe("validaciones del formulario", () => {
+    it("mostrar los mensajes de error si los campos del formulario estan vacios", async () => {
+        const handleSubmit = jest.fn();
+
+        render(<MembersForm onSubmit={handleSubmit} />)
+
+        userEvent.click(screen.getByRole('button', {
+            name: /crear/i
+        }))
+
+        await waitFor(() => {
+            expect(screen.getByText("El nombre es obligatorio")).toBeInTheDocument();
+            expect(screen.getByText("La imagen es obligatoria")).toBeInTheDocument();
+            expect(screen.getByText("La descripción es obligatorio")).toBeInTheDocument();
+            expect(screen.getByText("La url de Facebook es obligatorio")).toBeInTheDocument();
+            expect(screen.getByText("La url de LinkedIn es obligatorio")).toBeInTheDocument();
+        })
+    })
+
+    it("prevenir submit en caso que el formulario no este completado", async () => {
+        const handleClick = jest.fn();
+
+        render(<MembersForm onSubmit={handleClick} />)
+
+        userEvent.click(screen.getByRole('button', {
+            name: /crear/i
+        }))
+
+        await waitFor(() => {
+            expect(handleClick).not.toHaveBeenCalled();
+        })
+    })
+})
