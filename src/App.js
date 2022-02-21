976956ac82a8d20bc136f0065162413f8bb6a05a
pablorosti <pablorostirollatenis@gmail.com>
2022-02-21 17:02:52 -0300

    resolved error eslint

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import { publicRoute } from "./Components/Route/publicRoutes";
 import PrivateRoute from "./Components/Route/PrivateRoute";
 import LayoutPublic from "./Components/Layout/LayoutPublic";
 import Error404Page from "./Pages/Error404Page";
-
-
 const SchoolCampaign = lazy(() =>
   import(/* webpackChunkName: "LazySchoolCampaignPage"*/ "./Campaigns/School/SchoolCampaign")
 );
