43564d2aa3d24b58fdc84f2a619e60e883443d67
Ivan Salas <ivanedusalas@gmail.com>
2022-02-21 19:19:35 -0300

    fix: se remueve useffect

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import PropTypes from "prop-types";
