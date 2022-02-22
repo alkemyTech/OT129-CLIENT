094f42142290c3249b632a12f29d462f664f72a5
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-22 10:39:49 -0300

    Uncommented lines in the register submit

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -83,9 +83,9 @@ const RegisterForm = () => {
       name,
       email,
       password,
-      // address: address,
-      // latitude: map.lat,
-      // longitude: map.lng,
+      address: address,
+      latitude: map.lat,
+      longitude: map.lng,
     };
 
     dispatch(getRegistered(body));
