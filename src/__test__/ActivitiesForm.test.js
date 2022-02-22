394731d17872dd9c81f2fcc824377c3ae08488cf
pablorosti <pablorostirollatenis@gmail.com>
2022-02-22 00:22:56 -0300

    test: tests were added to validate if the validations of the form work, to validate that the edition and creation forms are rendered correctly and that submit cannot be done if the fields did not pass the validation

--- src/__test__/ActivitiesForm.test.js
+++ src/__test__/ActivitiesForm.test.js
@@ -1,102 +1,44 @@
 import React from "react";
-import { render, waitFor, screen, cleanup } from "@testing-library/react";
-import user from "@testing-library/user-event";
-import axiosMock from "ActivitiesFormMock";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import ActivitiesForm from "../Components/Activities/ActivitiesForm";
-import { activitiesAPI } from "../api/methods";
 
-const newActivity = {
-  name: "New Super Activity",
-  description: "",
-  image: new File(["hello"], "hello.png", { type: "image/png" }),
+const activity = {
+  id: 1,
+  name: "Pablo",
+  description: "descripcion",
+  image: "image",
 };
-const activityToUpload = {
-  category_id: null,
-  created_at: "2021-09-27T03:49:04.000000Z",
-  deleted_at: null,
-  description: "<p>test</p>",
-  group_id: null,
-  id: 618,
-  image: "http://ongapi.alkemy.org/storage/68PFo3kHqg.jpeg",
-  name: "test",
-  slug: null,
-  updated_at: "2021-09-27T03:49:04.000000Z",
-  user_id: null,
-};
-const updatedActivity = {
-  name: "Updated Super Activity",
-  description: "",
-  image: new File(["goodbye"], "goodbye.png", { type: "image/png" }),
-};
-const onSubmit = axiosMock.post;
+const decideAction = jest.fn();
 
-beforeEach(async () => {
-  onSubmit.mockClear();
-});
-afterEach(cleanup);
-
-describe("Can't submit if the form has errors", () => {
-  test("An error message will be showed if the fields are not filled", async () => {
-    await waitFor(() => {
-      render(<ActivitiesForm />);
-    });
-
-    axiosMock.post.mockResolvedValueOnce({ data: "Request failure" });
-    user.click(getSubmitButton());
+describe("<ActivitiesForm/>", () => {
+  test("should show validation errors", async () => {
+    render(<ActivitiesForm />);
+    userEvent.click(screen.getByTestId("btnSubmit"));
 
     await waitFor(() => {
-      expect(onSubmit).toHaveBeenCalledTimes(0);
-    });
-
-    expect(onSubmit).not.toHaveBeenCalledWith({});
-
-    await waitFor(() => {
-      expect(getNameErrorMessage()).toBeInTheDocument();
-      expect(getImageErrorMessage()).toBeInTheDocument();
+      expect(screen.getByText(/El nombre de la actividad es obligatorio/i)).toBeInTheDocument();
+      expect(screen.getByText(/Debe adjuntar una imagen/i)).toBeInTheDocument();
     });
   });
-});
 
-describe("Can create a new activity", () => {
-  test("When all required fields are complete, can make a form submit", async () => {
-    await waitFor(() => {
-      render(<ActivitiesForm />);
-    });
+  test("Should show create form", () => {
+    render(<ActivitiesForm />);
 
-    axiosMock.post.mockResolvedValueOnce({ data: "Request made successfully" });
+    expect(screen.getByText("CREAR")).toBeInTheDocument();
+  });
 
-    const sectionHeader = screen.getByRole("heading");
+  test("Should show edit form", () => {
+    render(<ActivitiesForm activity={activity} />);
 
-    expect(sectionHeader).toHaveTextContent("Add Activity");
+    expect(screen.getByText("EDITAR")).toBeInTheDocument();
+  });
 
-    user.type(getNameInput(), newActivity.name);
-    await waitFor(() => {
-      user.upload(getUploadImg(), newActivity.image);
-    });
+  test("Prevent form submission if fields are not validated", () => {
+    render(<ActivitiesForm decideAction={decideAction} />);
 
-    user.click(getSubmitButton());
-    await waitFor(() => {
-      expect(getNameInput().value).not.toBe(newActivity.name);
-      expect(getNameInput().value).toBe("");
-    });
+    userEvent.click(screen.getByTestId("btnSubmit"));
+    expect(decideAction.mock.calls).toHaveLength(0);
   });
 });
-
-function getNameInput() {
-  return screen.getByPlaceholderText(/name/i);
-}
-function getUploadImg() {
-  return screen.getByRole("button", {
-    name: /upload file/i,
-  });
-}
-function getSubmitButton() {
-  return screen.getByText(/save activity/i);
-}
-function getNameErrorMessage() {
-  return screen.getByText(/required/i);
-}
-function getImageErrorMessage() {
-  return screen.getByText(/you need to provide a file/i);
-}
