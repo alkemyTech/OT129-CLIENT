fa1ca197a786c47b1370e9e7d2cdf6d3d64ec598
pablorosti <pablorostirollatenis@gmail.com>
2022-02-22 17:33:02 -0300

    test: tests were added to validate that the put request is done correctly

--- src/__test__/ActivitiesForm.test.js
+++ src/__test__/ActivitiesForm.test.js
@@ -10,10 +10,12 @@ const activity = {
   description: "descripcion",
   image: new File(["hello"], "hello.png", { type: "image/png" }),
 };
+
 const mockdecideAction = jest.fn();
 
 describe("<ActivitiesForm/>", () => {
-  test("should show validation errors", async () => {
+  //Test que verifica que se muestren correctamente los mensajes de validacion
+  test("Should show validation errors", async () => {
     render(<ActivitiesForm />);
     userEvent.click(screen.getByTestId("btnSubmit"));
 
@@ -23,6 +25,7 @@ describe("<ActivitiesForm/>", () => {
     });
   });
 
+  //Test que verifica que si los inputs estan completos no se muestren los errores de validacion
   test("Should pass the validations and not show validation errors", async () => {
     render(<ActivitiesForm />);
 
@@ -38,6 +41,7 @@ describe("<ActivitiesForm/>", () => {
     });
   });
 
+  //Test para verificar si hay errores de validaciones no se haga correctamente la peticion
   test("Should show validation errors and not let the submit be done", async () => {
     render(<ActivitiesForm decideAction={mockdecideAction} />);
 
@@ -49,21 +53,22 @@ describe("<ActivitiesForm/>", () => {
     });
   });
 
+  //Test para ver que se renderice correctamente el formulario de crear una actividad
   test("Should show create form", () => {
     render(<ActivitiesForm />);
 
     expect(screen.getByText("CREAR")).toBeInTheDocument();
   });
 
+  //Test para ver que se renderice correctamente el formulario de editar una actividad
   test("Should show edit form", () => {
     render(<ActivitiesForm activity={activity} />);
 
     expect(screen.getByText("EDITAR")).toBeInTheDocument();
   });
 
-  test("should do the submit and create an activity correctly", () => {
-    const mockdecideAction = jest.fn();
-
+  //Test para verificar que se haya hecho correctamente la peticion post
+  test("Should do the submit and create an activity correctly", () => {
     render(<ActivitiesForm decideAction={mockdecideAction} />);
 
     waitFor(async () => {
@@ -71,12 +76,74 @@ describe("<ActivitiesForm/>", () => {
       const inputDescription = screen.findByTestId("inputDescription");
       const inputImage = screen.findByTestId("inputImage");
 
-      userEvent.type(inputName, "Prueba X");
-      userEvent.type(inputDescription, "Descripcion de prueba");
-      userEvent.type(inputImage, "miagendeprueba");
+      userEvent.type(inputName, activity.name);
+      userEvent.type(inputDescription, activity.description);
+      userEvent.upload(inputImage, activity.image);
       userEvent.click(screen.getByTestId("btnSubmit"));
       expect(mockdecideAction).toBeCalled();
-      expect(screen.findByText("La actividad se creó correctamente.")).toBeInTheDocument();
+      expect(screen.findByText(/La actividad se creó correctamente./i)).toBeInTheDocument();
+    });
+  });
+
+  //Test para verificar que no se haya hecho correctamente la peticion post
+  test("Should make the shipment and show the error that it could not be created correctly.", () => {
+    render(<ActivitiesForm decideAction={mockdecideAction} />);
+
+    waitFor(async () => {
+      const inputName = await screen.findByTestId("inputName");
+      const inputDescription = await screen.findByTestId("inputDescription");
+      const inputImage = await screen.findByTestId("inputImage");
+
+      userEvent.type(inputName, activity.name);
+      userEvent.type(inputDescription, activity.description);
+      userEvent.type(inputImage, activity.image);
+      userEvent.click(screen.getByTestId("btnSubmit"));
+      expect(mockdecideAction).toBeCalled();
+      const createdMessage = await screen.findByText(
+        /Lo sentimos. La actividad no pudo ser creada./i
+      );
+
+      expect(createdMessage).toBeInTheDocument();
+    });
+  });
+
+  //Test para verificar que se haya hecho correctamente la peticion put
+  test("Should do the submit and edit an activity correctly", () => {
+    render(<ActivitiesForm activity={activity} decideAction={mockdecideAction} />);
+
+    waitFor(async () => {
+      const inputName = screen.findByTestId("inputTitle");
+      const inputDescription = screen.findByTestId("inputDescription");
+      const inputImage = screen.findByTestId("inputImage");
+
+      userEvent.type(inputName, activity.name);
+      userEvent.type(inputDescription, activity.description);
+      userEvent.upload(inputImage, activity.image);
+      userEvent.click(screen.getByTestId("btnSubmit"));
+      expect(mockdecideAction).toBeCalled();
+      expect(screen.findByText(/La actividad id: 1 se editó correctamente/i)).toBeInTheDocument();
+    });
+  });
+
+  //Test para verificar que no se haya hecho correctamente la peticion put
+  test("Should make the shipment and show the error that it could not be edit correctly.", () => {
+    render(<ActivitiesForm activity={activity} decideAction={mockdecideAction} />);
+
+    waitFor(async () => {
+      const inputName = await screen.findByTestId("inputName");
+      const inputDescription = await screen.findByTestId("inputDescription");
+      const inputImage = await screen.findByTestId("inputImage");
+
+      userEvent.type(inputName, activity.name);
+      userEvent.type(inputDescription, activity.description);
+      userEvent.type(inputImage, activity.image);
+      userEvent.click(screen.getByTestId("btnSubmit"));
+      expect(mockdecideAction).toBeCalled();
+      const createdMessage = await screen.findByText(
+        /Lo sentimos! La actividad con id: 1 no pudo ser editada./i
+      );
+
+      expect(createdMessage).toBeInTheDocument();
     });
   });
 });
