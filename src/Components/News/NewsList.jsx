21fe4a5d55983f9389525da1dc8b6927d2748bdb
Emiliano <emiliano.quiroga093@gmail.com>
2022-02-22 09:24:17 -0300

    Renamed file and import

--- src/Components/News/NewsList.jsx
+++ src/Components/News/NewsList.jsx
@@ -3,14 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { fetchNews, selectorNews } from "../../features/News/news-slice";
 import TitleNav from "../TitleNav/TitleNav";
-import { useDebouceSearch } from "../../hooks/useDebouceSearch";
+import { useDebounceSearch } from "../../hooks/useDebounceSearch";
 import SearchInput from "../SearchInput/SearchInput";
 
 import NewsTable from "./NewsTable";
 
 const NewsList = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const searchValues = useDebouceSearch(searchTerm, 400);
+  const searchValues = useDebounceSearch(searchTerm, 400);
   const { news } = useSelector(selectorNews);
   const dispatch = useDispatch();
   const handleSearch = (e) => {
@@ -23,7 +23,11 @@ const NewsList = () => {
 
   return (
     <div className="container mt-5">
-      <TitleNav link="/backoffice/news/create" linkTitle="Crear" title="Novedades" />
+      <TitleNav
+        link="/backoffice/news/create"
+        linkTitle="Crear"
+        title="Novedades"
+      />
       <SearchInput
         handleSearch={handleSearch}
         title="Ingresa el nombre de la novedad que desea buscar"
