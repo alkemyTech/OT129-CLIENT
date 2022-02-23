c8e2c55f9378ccc18b91e10670155482a0f7ccd2
Fabricio Capiglioni <fabriciokpi@gmail.com>
2022-02-22 22:17:20 -0300

    add test for edit form

--- src/__test__/NewsForm.test.js
+++ src/__test__/NewsForm.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { editNews } from "../Services/NewsService";
 import NewsForm from "../Components/News/NewsForm";
 
 const newId = {
@@ -18,6 +17,12 @@ describe("<NewsForm/>", () => {
     render(<NewsForm newId={undefined} />);
     await waitFor(() => {
       expect(screen.getByText("AGREGAR NOVEDAD")).toBeInTheDocument();
+    });
+  });
+  it("Should show edit form", async () => {
+    render(<NewsForm newId={newId} />);
+    await waitFor(() => {
+      expect(screen.getByText("EDITAR NOTICIA")).toBeInTheDocument();
     });
   });
   it("Should not submit form if any field is'nt complete", async () => {
@@ -37,28 +42,9 @@ describe("<NewsForm/>", () => {
 
     await waitFor(() => {
       expect(screen.getByText(/El titulo es obligatorio/i)).toBeInTheDocument();
-      /*  expect(screen.getByText(/El contenido es obligatorio/i)).toBeInTheDocument(); */
+      expect(screen.getByText(/El contenido es obligatorio/i)).toBeInTheDocument();
       expect(screen.getByText(/La categoría es obligatoria/i)).toBeInTheDocument();
       expect(screen.getByText(/La imagen es obligatoria/i)).toBeInTheDocument();
-    });
-  });
-  it("Should do the submit and edit an activity correctly", () => {
-    render(<NewsForm newId={newId} />);
-
-    const inputName = screen.findByTestId("inputTitle");
-
-    const inputImage = screen.findByTestId("inputImage");
-    const inputCategory = screen.findByTestId("inputCategory");
-
-    waitFor(async () => {
-      userEvent.type(inputName, newId.name);
-
-      userEvent.type(inputCategory, newId.category_id);
-      userEvent.upload(inputImage, newId.image);
-      userEvent.click(screen.getByTestId("btnSubmit"));
-
-      expect(editNews).toBeCalled();
-      expect(screen.findByText(/La novedad se editó correctamente/i)).toBeInTheDocument();
     });
   });
 });
