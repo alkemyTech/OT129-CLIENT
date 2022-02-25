0439c1b4931f3493ebbcbd3dfb2def1790bef01b
Ana Galli <anaamgalli@gmail.com>
2022-02-25 20:58:12 -0300

    style: lastEvent

--- src/Components/LastEvent/LastEvent.js
+++ src/Components/LastEvent/LastEvent.js
@@ -4,7 +4,7 @@ import ReactPlayer from "react-player/youtube";
 
 const LastEvent = ({ title = "Ultimo Evento" }) => {
   return (
-    <div className="container d-flex justify-content-center align-items-center banner py-5">
+    <div className="container d-flex justify-content-center align-items-center banner mb-5">
       <div className="row justify-content-center">
         <h2 className="mb-5 title-banner">{title}</h2>
         <ReactPlayer controls url={"https://www.youtube.com/watch?v=4YnSk1gI_Oo"} volume="0 a 1" />
