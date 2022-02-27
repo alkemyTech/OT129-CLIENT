f8c4ae9c60b81e9fa12ee9fa28b9df527b323e69
Emiliano <82418581+emquiroga@users.noreply.github.com>
2022-02-27 18:38:57 -0300

    FEAT: Mobile Menu (#165)
    * Added transition
    
    * Stored constants in common file
    
    * Added MobileNav component
    
    * Updated styles for common use
    
    * Created new component and styles
    
    * Added active class

---
+++ src/Components/Header/MobileNav.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Popup from "reactjs-popup";
+import { NavLink, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { selectAuth } from "../../features/auth/authSlice";
+import { logout } from "../../features/auth/authSlice";
+import { menuPublic } from "../../constants";
+
+import styles from "./MobileNav.module.css";
+
+const MobileNav = () => {
+  const dispatch = useDispatch();
+
+  const {
+    auth,
+    user: { role_id },
+  } = useSelector(selectAuth);
+
+  return (
+    <Popup
+      modal
+      trigger={
+        <button className={styles.menuButton}>
+          <i className="fa fa-bars" />
+        </button>
+      }
+    >
+      {(close) => (
+        <div className={styles.modalContainer}>
+          <button className={styles.closeMenu} onClick={close}>
+            <i className="fas fa-times" />{" "}
+          </button>
+          <nav className={styles.mobileNav}>
+            {auth ? (
+              <>
+                <Link
+                  className={styles.authLink}
+                  data-bs-placement="bottom"
+                  data-bs-toggle="tooltip"
+                  title="Cerrar Sesión"
+                  to="/"
+                  onClick={() => {
+                    dispatch(logout());
+                    close();
+                  }}
+                >
+                  Logout
+                </Link>
+                {role_id === 2 && (
+                  <Link
+                    className="general-btn stroke-btn text-decoration-none"
+                    to="/donar"
+                    onClick={close}
+                  >
+                    Donar
+                  </Link>
+                )}
+              </>
+            ) : (
+              <>
+                <Link
+                  className={styles.authLink}
+                  data-bs-placement="bottom"
+                  data-bs-toggle="tooltip"
+                  title="Iniciar Sesión"
+                  to="/login"
+                  onClick={close}
+                >
+                  Login
+                </Link>
+                <Link className={styles.authLink} to="/registro" onClick={close}>
+                  Registro
+                </Link>
+              </>
+            )}
+            <hr className={styles.menuDivider} />
+            {menuPublic.map((item) => {
+              return (
+                <NavLink
+                  key={item.id.toString()}
+                  exact
+                  activeClassName={styles.active}
+                  className={styles.navLink}
+                  target={item.target}
+                  to={item.link.toString()}
+                  onClick={close}
+                >
+                  {item.name}
+                </NavLink>
+              );
+            })}
+          </nav>
+        </div>
+      )}
+    </Popup>
+  );
+};
+
+export default MobileNav;
