354e008f01a20e1f6695857a99a9c308284f765d
Ana Galli <97997049+anagalli@users.noreply.github.com>
2022-02-27 18:36:15 -0300

    Refactor Activities and News Pages (#163)
    * remove unused component
    
    * refactor: change component name to english
    
    * refactor: structure NewsCards & ActivitiesCards
    
    * styles: add css classes to SearchInput
    
    * refactor: update NewsSlice reducers
    
    * refactor: change component name in NewsPage
    
    * styles: add styles to Activities & News Cards
    
    * styles: responsive design for cards
    
    * style: homeNewsCards
    
    * style: lastEvent
    
    * style: activitiesCards responsive
    
    * style: NewsCards responsive

--- src/Containers/NewsCards/NewsCards.js
+++ src/Containers/NewsCards/NewsCards.js
@@ -3,16 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 
 import Card from "../../Components/Card/Card";
-import StatusHandler from "../../Components/StatusHandler/StatusHandler";
 import { fetchNews, selectorNews } from "../../features/News/news-slice";
 import { useDebounceSearch } from "../../hooks/useDebounceSearch";
 import SearchInput from "../../Components/SearchInput/SearchInput";
+
+import styles from "./NewsCards.module.css";
 
 const NewsCards = () => {
   const [search, setSearch] = useState("");
   const searchValue = useDebounceSearch(search);
   const dispatch = useDispatch();
-  const { news, status } = useSelector(selectorNews);
+  const { news } = useSelector(selectorNews);
 
   const changeHandler = (e) => {
     setSearch(e.target.value);
@@ -23,28 +24,23 @@ const NewsCards = () => {
   }, [dispatch, searchValue]);
 
   return (
-    <div className="container mt-3">
-      <div className="w-25 mb-3">
-        <SearchInput handleSearch={changeHandler} title="Busca tus novedades por NOMBRE" />
-      </div>
-      <div className="row">
-        <StatusHandler status={status} />
-        <div className="container-cards">
-          {news?.length != 0 ? (
-            news?.map((el) => (
-              <Card
-                key={el.id}
-                description={el.content}
-                id={el.id}
-                image={el.image}
-                title={el.name}
-                url={`./novedades`}
-              />
-            ))
-          ) : (
-            <p>No hay novedades</p>
-          )}
-        </div>
+    <div className={`container ${styles.containerNewsPage}`}>
+      <SearchInput handleSearch={changeHandler} title="Buscar novedad" />
+      <div className={`mt-5 ${styles.newsCardsContainer}`}>
+        {news?.length != 0 ? (
+          news?.map((el) => (
+            <Card
+              key={el.id}
+              description={el.content}
+              id={el.id}
+              image={el.image}
+              title={el.name}
+              url={`./novedades`}
+            />
+          ))
+        ) : (
+          <p>No hay novedades</p>
+        )}
       </div>
     </div>
   );
