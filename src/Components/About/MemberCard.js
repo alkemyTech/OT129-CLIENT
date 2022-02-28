4eb6f626d4bd9d7d88e72dcc9dcc2998b4dd92d2
Ana Galli <anaamgalli@gmail.com>
2022-02-28 13:06:12 -0300

    style: memberCard responsive design

--- src/Components/About/MemberCard.js
+++ src/Components/About/MemberCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./MemberCard.css";
+import styles from "./MemberCard.module.css";
 
 const MemberCard = ({ name, image, description, facebookUrl, linkedinUrl }) => {
   return (
-    <div className="card">
-      <img alt="Member Image" className="card-img-top" src={image} />
-      <div className="card-body">
+    <div className={`card ${styles.memberCard}`}>
+      <img alt="Member Image" className={styles.cardImgTop} src={image} />
+      <div className={`card-body`}>
         <h5 className="card-title">{name}</h5>
         <p dangerouslySetInnerHTML={{ __html: `${description}` }} className="card-text" />
         <div className="d-flex justify-content-around">
