b0b023f4ec0d3534157cd1856dea0a69332714ad
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-01 16:00:49 -0300

    Removed import due warning

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../FormStyles.css";
 import { Formik, ErrorMessage, Form } from "formik";
 import * as Yup from "yup";
