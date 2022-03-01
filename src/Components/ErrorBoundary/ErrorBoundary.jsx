84d72a31659aa9a32bc2a74b2e960a17dc3e9ca9
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-01 12:52:22 -0300

    Removed infoStack

--- src/Components/ErrorBoundary/ErrorBoundary.jsx
+++ src/Components/ErrorBoundary/ErrorBoundary.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 import propTypes from "prop-types";
 
+const RefreshButton = () => {
+  const handleClick = () => {
+    window.location.reload();
+  };
+
+  return (
+    <button
+      className="btn btn-info"
+      style={{ margin: "auto", width: "120px" }}
+      onClick={handleClick}
+    >
+      Recargar
+    </button>
+  );
+};
+
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: null, errorInfo: null };
+    this.state = { error: null };
   }
 
-  componentDidCatch(error, errorInfo) {
+  componentDidCatch(error) {
     this.setState({
-      error: error,
-      errorInfo: errorInfo,
+      error,
     });
   }
 
   render() {
-    if (this.state.errorInfo) {
+    if (this.state.error) {
       return (
         <div className="container mt-3">
-          <div className="alert alert-danger">Ups! Ha ocurrido un error inesperado</div>
-          <details style={{ whiteSpace: "pre-wrap" }}>
-            {this.state.error && this.state.error.toString()}
-            <br />
-            {this.state.errorInfo.componentStack}
-          </details>
+          <div className="d-flex flex-column justify-content-center">
+            <div className="alert alert-danger">
+              <h1 className="text-center">Ups! Ha ocurrido un error inesperado</h1>
+            </div>
+            <RefreshButton />
+          </div>
         </div>
       );
     }
