750051e9bd71f2c32b70b39a757568a4270c3247
FabricioCapiglioni <78228416+FabricioCapiglioni@users.noreply.github.com>
2022-03-01 00:42:24 -0300

    Refactor extend slide requests (#180)
    * fix: added functionality deleted slide and refactor archive slideServices
    
    * fix: added route edit slides
    
    * edit putSlides function
    
    * add missing alerts import
    
    * Add missing reducers
    
    * feat: add onDelete function
    
    * feat: add redux and handleSubmit function
    
    * remove console.log
    
    * refactor: move onDelete function into container
    
    * refactor: move submit into the form container
    
    Co-authored-by: pablorosti <pablorostirollatenis@gmail.com>

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,88 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import React, { useEffect, useState } from "react";
+import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import PropTypes from "prop-types";
 
 import { toBase64 } from "../../utils/toBase64";
-import { getSlides, postSlides, putSlides } from "../../Services/SlidesServices";
 import Alert from "../Alert/Alert";
 
-const SlidesForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: "",
-    description: "",
-    order: 0,
-    image: "",
-  });
+const SlidesForm = ({ slide = {}, slides = [], handleSubmit }) => {
   const [ordersList, setOrdersList] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [slideImage, setSlideImage] = useState("");
 
-  const { id } = useParams();
-
-  const getOrdersList = async () => {
-    const res = await getSlides();
-    let data = res.data.data;
-    // arreglo de order utilizados
-    const orderBlackList = data
-      .map((data) => data.order)
-      .filter((order) => order !== initialValues.order);
-
-    setOrdersList(orderBlackList);
+  const initialValues = {
+    name: slide.name || "",
+    description: slide.description || "",
+    order: slide.order || undefined,
+    image: slide.image || "",
   };
 
-  const getSlideById = async (id) => {
-    setLoading(true);
-
-    const res = await getSlides(id);
-
-    if (res.data.success) {
-      const { name, description, order, image } = res.data.data;
-
-      setInitialValues({
-        name,
-        description,
-        order,
-        image,
-      });
-    } else {
-      const { status } = res.data;
-
-      alert(status.message);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    if (id) {
-      getSlideById(id);
-    }
-    // se obtiene un arreglo de orders ya usados
-    getOrdersList();
-  }, []);
-
-  const handleSubmit = async (formValues) => {
-    let { image, ...rest } = formValues;
-
-    if (typeof image === "object") {
-      image = await toBase64(image);
-      formValues = {
-        image,
-        ...rest,
-      };
-    }
-
-    if (id) {
-      await putSlides(id, formValues);
-    } else {
-      await postSlides(formValues);
-    }
-  };
-
-  const inputFileRef = useRef();
   const validations = Yup.object({
     name: Yup.string()
       .min(4, "Debe tener al menos 4 caracteres")
@@ -92,126 +28,127 @@ const SlidesForm = () => {
     order: Yup.number()
       .moreThan(0, "Debe ser un numero mayor o igual a cero")
       .required("Este campo es obligatorio")
-      .integer()
+
       .notOneOf(ordersList, "Numero de orden ya esta en uso"),
     image: Yup.string().required("Este campo es obligatorio"),
   });
 
+  const getOrdersList = () => {
+    const orderBlackList = slides
+      .map((slide) => slide.order)
+      .filter((order) => order !== initialValues.order);
+
+    setOrdersList(orderBlackList);
+  };
+
+  useEffect(() => {
+    getOrdersList();
+    if (slide.id) {
+      setSlideImage(slide.image);
+    }
+  }, [slide]);
+
   return (
     <>
-      {loading ? (
-        <p>LOADING...</p>
-      ) : (
-        <Formik
-          enableReinitialize
-          initialValues={initialValues}
-          validationSchema={validations}
-          onSubmit={async (values, { resetForm }) => {
-            const resultbase = await toBase64(values.image);
-            let formValues = {
-              name: values.name,
-              description: values.description,
-              order: values.order,
-              image: resultbase,
-            };
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validations}
+        onSubmit={async (values) => {
+          const resultbase = await toBase64(values.image);
+          let formValues = {
+            ...values,
+            image: resultbase,
+          };
 
-            await handleSubmit(formValues);
-
-            // limpio el input file
-            inputFileRef.current.value = "";
-
-            resetForm();
-          }}
-        >
-          {({ setFieldValue }) => (
-            <Form className="form-backoffice">
-              <div className="form-group">
-                <label className="form-label fw-bold mt-1 fw-bold" htmlFor="name">
-                  Titulo:
-                </label>
-                <Field
-                  className="form-control form-control-sm w-100 mb-3"
-                  id="name"
-                  name="name"
-                  placeholder="Titulo"
-                  type="text"
-                />
-
-                <ErrorMessage component={Alert} name="name" />
-              </div>
+          await handleSubmit(formValues);
+        }}
+      >
+        {(formik) => (
+          <Form className="form-backoffice">
+            <div className="form-group">
+              <label className="form-label fw-bold mt-1 fw-bold" htmlFor="name">
+                Titulo:
+              </label>
+              <input
+                className="form-control form-control-sm w-100 mb-3"
+                name="name"
+                placeholder={initialValues.name || "Título"}
+                type="text"
+                value={initialValues.name}
+                {...formik.getFieldProps("name")}
+              />
+              <ErrorMessage component={Alert} name="name" />
+            </div>
+            <div className="form-group mb-3">
+              <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="fdescription">
+                Descripcion:
+              </label>
+              <CKEditor
+                data={initialValues.description}
+                editor={ClassicEditor}
+                id="description"
+                onChange={(event, editor) => formik.setFieldValue("description", editor.getData())}
+              />
+              <ErrorMessage component={Alert} name="description" />
+            </div>
+            <div className="form-group">
+              <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="order">
+                Numero de orden:
+              </label>
+              <input
+                className="form-control form-control-sm w-100 mb-3"
+                min="0"
+                name="order"
+                placeholder={initialValues.order || "Ingrese un número de orden"}
+                type="number"
+                onChange={(e) => {
+                  formik.setFieldValue("order", parseInt(e.currentTarget.value));
+                }}
+              />
+              <ErrorMessage component={Alert} name="order" />
+            </div>
+            <div className="form-group">
+              <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="order">
+                Cargar Imagen:
+              </label>
+              <input
+                accept=".jpg, .png"
+                className="form-control form-control-sm w-100 mb-3"
+                name="image"
+                type="file"
+                onChange={(e) => {
+                  formik.setFieldValue("image", e.currentTarget.files[0]);
+                  setSlideImage(URL.createObjectURL(e.currentTarget.files[0]));
+                }}
+              />
+              <ErrorMessage component={Alert} name="image" />
+            </div>
+            {slideImage && (
               <div className="form-group mb-3">
-                <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="fdescription">
-                  Descripcion:
-                </label>
-                <Field name="description">
-                  {({ field }) => (
-                    <>
-                      <CKEditor
-                        data={field.value}
-                        editor={ClassicEditor}
-                        onChange={(event, editor) => {
-                          setFieldValue(field.name, editor.getData());
-                        }}
-                      />
-                    </>
-                  )}
-                </Field>
-                <ErrorMessage component={Alert} name="description" />
+                <label className="form-label fw-bold mt-1 fw-bold mt-1">(Imagen actual)</label>
+                <img alt="Imagen actual" className="preview-image" src={slideImage} />
               </div>
-              <div className="form-group">
-                <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="order">
-                  Numero de orden:
-                </label>
-                <Field
-                  className="form-control form-control-sm w-100 mb-3"
-                  id="order"
-                  min="0"
-                  name="order"
-                  placeholder="ingrese un numero"
-                  type="number"
-                  onChange={(e) => {
-                    setFieldValue("order", parseInt(e.currentTarget.value));
-                  }}
-                />
-                <ErrorMessage component={Alert} name="order" />
-              </div>
-              <div className="form-group">
-                <label className="form-label fw-bold mt-1 fw-bold mt-1" htmlFor="order">
-                  Cargar Imagen:
-                </label>
-                <input
-                  ref={inputFileRef}
-                  accept=".jpg, .png"
-                  className="form-control form-control-sm w-100 mb-3"
-                  type="file"
-                  onChange={(e) => {
-                    setFieldValue("image", e.currentTarget.files[0]);
-                  }}
-                />
-                <ErrorMessage component={Alert} name="image" />
-              </div>
-              <button className="submit-btn" type="submit">
-                <span
-                  aria-hidden="true"
-                  className={loading ? "spinner-border spinner-border-sm" : null}
-                  role="status"
-                />
-                {id === undefined ? "AGREGAR" : "EDITAR"}
-              </button>
-            </Form>
-          )}
-        </Formik>
-      )}
+            )}
+            <button className="submit-btn" type="submit">
+              {slide.id === undefined ? "AGREGAR" : "EDITAR"}
+            </button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 };
 
 SlidesForm.propTypes = {
-  id: PropTypes.number,
-  name: PropTypes.string,
-  descripcion: PropTypes.string,
-  image: PropTypes.string,
-  order: PropTypes.number,
+  slides: PropTypes.array,
+  handleSubmit: PropTypes.func,
+  slide: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    descripcion: PropTypes.string,
+    image: PropTypes.string,
+    order: PropTypes.number,
+  }),
 };
 
 export default SlidesForm;
