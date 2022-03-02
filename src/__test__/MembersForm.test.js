081beb25bdf8747b8d1d172aa8e5702121b34d2e
Antony Fagundez <jhonfagundez@gmail.com>
2022-03-02 09:57:36 -0300

    test: fix import

--- src/__test__/MembersForm.test.js
+++ src/__test__/MembersForm.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import mockReactRedux from "mock-react-redux";
+import { mockReactRedux } from "mock-react-redux";
 
 import MembersForm from "../Components/Members/MembersForm";
 import { newMember } from "../features/Members/membersSlice";
