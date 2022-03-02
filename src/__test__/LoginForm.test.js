c54aca2116cdd66c8b1406d4a0a70b0918648d55
Your Name <you@example.com>
2022-03-02 06:21:05 -0300

    fix data-attributes

--- src/__test__/LoginForm.test.js
+++ src/__test__/LoginForm.test.js
@@ -19,7 +19,7 @@ describe("<LoginForm /> ", () => {
 
     render(<LoginForm onSubmit={dispatch} />);
 
-    userEvent.click(screen.getByTestId("LoggedButton"));
+    userEvent.click(screen.getByTestId("loggedButton"));
 
     await waitFor(() => {
       expect(screen.getByText("El campo EMAIL es obligatorio")).toBeInTheDocument();
@@ -32,7 +32,7 @@ describe("<LoginForm /> ", () => {
 
     render(<LoginForm onSubmit={dispatch} />);
 
-    userEvent.click(screen.getByTestId("LoggedButton"));
+    userEvent.click(screen.getByTestId("loggedButton"));
 
     await waitFor(() => {
       expect(dispatch).not.toHaveBeenCalled();
@@ -49,7 +49,7 @@ describe("<LoginForm /> ", () => {
     userEvent.type(screen.getByPlaceholderText("Ingresa tu email"), "Flash123@gmail.com");
     userEvent.type(screen.getByPlaceholderText("Ingresa tu contraseña"), "Flash123@");
 
-    userEvent.click(screen.getByTestId("LoggedButton"));
+    userEvent.click(screen.getByTestId("loggedButton"));
 
     mockAxios.post = jest.fn().mockResolvedValue({
       data: {
@@ -82,7 +82,7 @@ describe("<LoginForm /> ", () => {
 
     userEvent.type(screen.getByPlaceholderText("Ingresa tu email"), "Flash123@gmail.com");
     userEvent.type(screen.getByPlaceholderText("Ingresa tu contraseña"), "Flash123@");
-    userEvent.click(screen.getByTestId("LoggedButton"));
+    userEvent.click(screen.getByTestId("loggedButton"));
 
     mockAxios.post = jest.fn().mockResolvedValue({
       data: {
