e846c266617cd3961d5f3e06cfb0495d81401337
Antony Fagundez <jhonfagundez@gmail.com>
2022-03-02 00:29:42 -0300

    test: remove unnecesary prop on mock ckeditor

--- src/__test__/__mocks__/@ckeditor/ckeditor5-react.js
+++ src/__test__/__mocks__/@ckeditor/ckeditor5-react.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 // eslint-disable-next-line react/prop-types
 export const CKEditor = ({ config, data, id, onChange }) => {
@@ -8,7 +7,6 @@ export const CKEditor = ({ config, data, id, onChange }) => {
       config={config}
       data={data}
       data-testid="inputDescription"
-      editor={ClassicEditor}
       id={id}
       onChange={onChange}
     />
