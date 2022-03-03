79294c7792474b3a98d80b0bd9d8f9267b6b2622
Your Name <you@example.com>
2022-03-02 23:17:30 -0300

    editing footer styles

--- src/Campaigns/Toys/Footer.js
+++ src/Campaigns/Toys/Footer.js
@@ -1,95 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./FooterToys.css";
-import PropTypes from "prop-types";
 
-import ONGLogo from "../../assets/onglogo.png";
+import logoONG from "../../assets/onglogo.png";
 
-const Footer = ({
-  facebook = "https://facebook.com",
-  twitter = "https://twitter.com/ONGSomosMas",
-  linkedin = "https://linkedin.com/in/somos-mas-85b310224/?trk=people-guest_people_search-card&originalSubdomain=ar",
-  instagram = "http://instagram.com",
-}) => {
+const Footer = () => {
   return (
-    <footer className="container-fluid container-footer">
-      <div className="row p-0">
-        <div className="col-sm-4 col-xxl-3 logo-container">
-          <nav className="nav">
-            <Link className="link-nav" to="/">
-              <img className="logo" src={ONGLogo} />
+    <footer className="text-center container-footer">
+      <div className="container p-3">
+        <div className="row d-flex align-items-center">
+          <img alt="logo ong" className="col-12 col-sm-4 col-md-4 col-xl-3" src={logoONG} />
+
+          <div className="d-none d-sm-block col-sm-4 col-md-4 col-xl-3">
+            <h4>Sitio web</h4>
+            <a
+              className="text-blue"
+              href="https://ONGSomosMas.com.ar"
+              rel="noreferrer"
+              target="_blank"
+            >
+              www.somosmas.ong
+            </a>
+          </div>
+
+          <div className="d-none d-xl-block col-xl-3">
+            <h4>Campañas</h4>
+            <Link className="text-blue" to="/toys-campaign">
+              Otras campañas
             </Link>
+          </div>
+
+          <nav className="d-flex justify-content-center align-items-center mt-4 mt-sm-0 d-md-block col-12 col-sm-4 col-md-4 col-xl-3">
+            <div className="m-1 d-md-flex align-items-center">
+              <a
+                className="containerImage "
+                href="https://facebook.com/ONGSomosMas"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-facebook-f " />
+              </a>
+              <p className="d-none d-md-inline text-blue m-1">facebook</p>
+            </div>
+
+            <div className="m-1 d-md-flex align-items-center ">
+              <a
+                className="containerImage "
+                href="https://twitter.com/ONGSomosMas"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-twitter" />
+              </a>
+              <p className="d-none d-md-inline text-blue m-1">Twitter</p>
+            </div>
+
+            <div className="m-1 d-md-flex align-items-center">
+              <a
+                className="containerImage "
+                href="https://instagram.com/ONGSomosMas"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-instagram " />
+              </a>
+              <p className="d-none d-md-inline text-blue m-1">Instagram</p>
+            </div>
+
+            <div className="m-1 d-md-flex align-items-center">
+              <a
+                className="containerImage "
+                href="https://linkedin.com/ONGSomosMas"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i className="fab fa-linkedin-in " />
+              </a>
+              <p className="d-none d-md-inline text-blue m-1">Linkedin</p>
+            </div>
           </nav>
         </div>
-        <div className="d-none d-sm-flex text-access-container col-sm-4 col-xxl-3">
-          <div className="text-access">
-            <h3>Sitio Web</h3>
-            <nav className="nav">
-              <Link className="link-nav text-decoration-none" to="/">
-                www.somosmas.ong
-              </Link>
-            </nav>
-          </div>
-        </div>
-        <div className="d-none d-xxl-flex text-access-container col-sm-4 col-xxl-3">
-          <div className="text-access">
-            <h3>Campañas</h3>
-            <nav className="nav">
-              <Link className="link-nav text-decoration-none" to="/school-campaign">
-                Escuelas
-              </Link>
-            </nav>
-          </div>
-        </div>
-        <div className="row-social col-sm-4 col-xxl-3">
-          <div className="socials-icons-container">
-            <div className="icon-container">
-              <a className="social-link " href={facebook} rel="noreferrer" target="_blank">
-                <div className="social-icon">
-                  <i className="fab fa-facebook-f" />
-                </div>
-                <p className="d-none d-lg-flex icon-text ">@somosmas</p>
-              </a>
-            </div>
-            <div className="icon-container">
-              <a className="social-link " href={twitter} rel="noreferrer" target="_blank">
-                <div className="social-icon">
-                  <i className="fab fa-twitter" />
-                </div>
-                <p className="d-none d-lg-flex icon-text ">@somosmas</p>
-              </a>
-            </div>
-            <div className="icon-container">
-              <a className="social-link " href={instagram} rel="noreferrer" target="_blank">
-                <div className="social-icon">
-                  <i className="fab fa-instagram" />
-                </div>
-                <p className="d-none d-lg-flex icon-text ">@somosmas</p>
-              </a>
-            </div>
-            <div className="icon-container">
-              <a className="social-link " href={linkedin} rel="noreferrer" target="_blank">
-                <div className="social-icon">
-                  <i className="fab fa-linkedin-in" />
-                </div>
-                <p className="d-none d-lg-flex icon-text ">@somosmas</p>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row copy mt-3 mt-lg-5 p-0">
-        <span>2022 by Alkemy. All Rights Reserved</span>
       </div>
     </footer>
   );
-};
-
-Footer.propTypes = {
-  facebook: PropTypes.string,
-  twitter: PropTypes.string,
-  instagram: PropTypes.string,
-  linkedin: PropTypes.string,
 };
 
 export default Footer;
